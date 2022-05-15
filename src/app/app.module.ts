57163b4a62066115487f1757fcceb62857b79af1
Krzysztof Pajk <chris.araneo@gmail.com>
2022-05-15 15:19:55 +0200

    Added Material UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,38 @@
+import { LayoutModule } from '@angular/cdk/layout';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxElectronModule } from 'ngx-electron';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+const MATERIAL_MODULES = [
+  LayoutModule,
+  MatButtonModule,
+  MatCardModule,
+  MatGridListModule,
+  MatIconModule,
+  MatListModule,
+  MatMenuModule,
+  MatSidenavModule,
+  MatToolbarModule,
+];
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NgxElectronModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxElectronModule,
+    ...MATERIAL_MODULES,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
