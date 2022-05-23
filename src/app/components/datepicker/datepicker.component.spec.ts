92ef8d22e63d7dc6ec1028c520f6cc4157685399
Krzysztof Pajk <chris.araneo@gmail.com>
2022-05-23 19:06:48 +0200

    Datepicker component reformatting

--- src/app/components/datepicker/datepicker.component.spec.ts
+++ src/app/components/datepicker/datepicker.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DatepickerComponent } from './datepicker.component';
 
 describe('DatepickerComponent', () => {
@@ -8,9 +7,8 @@ describe('DatepickerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DatepickerComponent ]
-    })
-    .compileComponents();
+      declarations: [DatepickerComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
