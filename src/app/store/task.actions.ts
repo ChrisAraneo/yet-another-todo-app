e37aee8cb5716c51d3eb353f0e59480f303a40e5
Krzysztof Pajk <chris.araneo@gmail.com>
2022-06-01 21:25:01 +0200

    setTasks action in store

--- src/app/store/task.actions.ts
+++ src/app/store/task.actions.ts
@@ -5,13 +5,9 @@ import { Task } from '../shared/model/task.type';
 const CREATE_TASK = 'CREATE_TASK';
 const REMOVE_TASK = 'REMOVE_TASK';
 const UPDATE_TASK_STATE = 'UPDATE_TASK_STATE';
+const SET_TASKS = 'SET_TASKS';
 
 export const createTask = createAction(CREATE_TASK, props<{ task: Task }>());
-export const removeTask = createAction(
-  REMOVE_TASK,
-  props<{ taskId: string }>()
-);
-export const updateTaskState = createAction(
-  UPDATE_TASK_STATE,
-  props<{ taskId: string; newState: TaskState }>()
-);
+export const removeTask = createAction(REMOVE_TASK, props<{ taskId: string }>());
+export const updateTaskState = createAction(UPDATE_TASK_STATE, props<{ taskId: string; newState: TaskState }>());
+export const setTasks = createAction(SET_TASKS, props<{ tasks: Task[] }>());
--- src/app/store/task.reducer.ts
+++ src/app/store/task.reducer.ts
@@ -1,18 +1,15 @@
 import { createReducer, on } from '@ngrx/store';
 import { Task } from '../shared/model/task.type';
-import { createTask, removeTask, updateTaskState } from './task.actions';
+import { createTask, removeTask, setTasks, updateTaskState } from './task.actions';
 
 export const initialState: Task[] = [];
 
 export const tasksReducer = createReducer(
   initialState,
   on(createTask, (state, { task }) => [...state, task]),
-  on(removeTask, (state, { taskId }) =>
-    [...state].filter((task) => task.id !== taskId)
-  ),
+  on(removeTask, (state, { taskId }) => [...state].filter((task) => task.id !== taskId)),
   on(updateTaskState, (state, { taskId, newState }) =>
-    [...state].map((task) =>
-      task.id === taskId ? { ...task, state: newState } : task
-    )
-  )
+    [...state].map((task) => (task.id === taskId ? { ...task, state: newState } : task)),
+  ),
+  on(setTasks, (_, { tasks }) => [...tasks]),
 );
