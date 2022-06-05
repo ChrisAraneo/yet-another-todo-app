3e0b0645078b97146f37b85e62a3600b9cabc654
Krzysztof Pajk <chris.araneo@gmail.com>
2022-06-05 21:16:12 +0200

    Datepicker component name change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { StoreModule } from '@ngrx/store';
 import { NgxElectronModule } from 'ngx-electron';
 import { MaterialModule } from 'src/material.module';
 import { AppComponent } from './app.component';
-import { DatepickerComponent } from './components/datepicker/datepicker.component';
+import { DatePickerComponent } from './components/date-picker/date-picker.component';
 import { TaskCardComponent } from './components/task-card/task-card.component';
 import { TaskStateIconComponent } from './components/task-state-icon/task-state-icon.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
@@ -17,7 +17,7 @@ import { TasksService } from './services/tasks.service';
 import { tasksReducer } from './store/task.reducer';
 
 @NgModule({
-  declarations: [AppComponent, ToolbarComponent, TaskCardComponent, DatepickerComponent, TaskStateIconComponent],
+  declarations: [AppComponent, ToolbarComponent, TaskCardComponent, DatePickerComponent, TaskStateIconComponent],
   imports: [
     StoreModule.forRoot({ tasks: tasksReducer }),
     MaterialModule,
