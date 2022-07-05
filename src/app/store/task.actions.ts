c1f014da68c2242e9368f37b3feeac2c80a3c4a3
Krzysztof Pajk <chris.araneo@gmail.com>
2022-07-05 21:56:54 +0200

    Update task end date action & reducer

--- src/app/store/task.actions.ts
+++ src/app/store/task.actions.ts
@@ -4,10 +4,12 @@ import { Task } from '../shared/model/task.type';
 
 const CREATE_TASK = 'CREATE_TASK';
 const REMOVE_TASK = 'REMOVE_TASK';
-const UPDATE_TASK_STATE = 'UPDATE_TASK_STATE';
 const SET_TASKS = 'SET_TASKS';
+const UPDATE_TASK_STATE = 'UPDATE_TASK_STATE';
+const UPDATE_TASK_END_DATE = 'UPDATE_TASK_END_DATE';
 
 export const createTask = createAction(CREATE_TASK, props<{ task: Task }>());
 export const removeTask = createAction(REMOVE_TASK, props<{ taskId: string }>());
-export const updateTaskState = createAction(UPDATE_TASK_STATE, props<{ taskId: string; newState: TaskState }>());
 export const setTasks = createAction(SET_TASKS, props<{ tasks: Task[] }>());
+export const updateTaskState = createAction(UPDATE_TASK_STATE, props<{ taskId: string; newState: TaskState }>());
+export const updateTaskEndDate = createAction(UPDATE_TASK_END_DATE, props<{ taskId: string; endDate?: Date }>());
--- src/app/store/task.reducer.ts
+++ src/app/store/task.reducer.ts
@@ -1,6 +1,6 @@
 import { createReducer, on } from '@ngrx/store';
 import { Task } from '../shared/model/task.type';
-import { createTask, removeTask, setTasks, updateTaskState } from './task.actions';
+import { createTask, removeTask, setTasks, updateTaskEndDate, updateTaskState } from './task.actions';
 
 export const initialState: Task[] = [];
 
@@ -8,8 +8,11 @@ export const tasksReducer = createReducer(
   initialState,
   on(createTask, (state, { task }) => [...state, task]),
   on(removeTask, (state, { taskId }) => [...state].filter((task) => task.id !== taskId)),
+  on(setTasks, (_, { tasks }) => [...tasks]),
   on(updateTaskState, (state, { taskId, newState }) =>
     [...state].map((task) => (task.id === taskId ? { ...task, state: newState } : task)),
   ),
-  on(setTasks, (_, { tasks }) => [...tasks]),
+  on(updateTaskEndDate, (state, { taskId, endDate }) =>
+    [...state].map((task) => (task.id === taskId ? { ...task, endDate } : task)),
+  ),
 );
