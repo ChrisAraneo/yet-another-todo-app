26d2f929fe20f98072b5fc264f629d2c1172ca59
Krzysztof Pajk <chris.araneo@gmail.com>
2022-12-20 22:36:08 +0100

    Side navigation animation, minor refactoring

--- yet-another-todo-app-frontend/src/app/components/side-navigation/side-navigation.component.ts
+++ yet-another-todo-app-frontend/src/app/components/side-navigation/side-navigation.component.ts
@@ -1,14 +1,37 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'yata-side-navigation',
   templateUrl: './side-navigation.component.html',
   styleUrls: ['./side-navigation.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state(
+        'open',
+        style({
+          width: '256px',
+          borderRight: '1px solid black',
+        })
+      ),
+      state(
+        'closed',
+        style({
+          width: '0px',
+          borderRight: '0px solid black',
+        })
+      ),
+      transition('open => closed', [animate('0.25s')]),
+      transition('closed => open', [animate('0.25s')]),
+    ]),
+  ],
 })
-export class SideNavigationComponent implements OnInit {
+export class SideNavigationComponent {
   @Input() isOpened: boolean = true;
-
-  constructor() {}
-
-  ngOnInit(): void {}
 }
