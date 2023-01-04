e6fb191266ea2fb206691f421748f7d1e54d071e
Krzysztof Pajk <chris.araneo@gmail.com>
2023-01-04 23:21:59 +0100

    Task card component update

--- yet-another-todo-app-frontend/src/app/components/timeline/task-card/task-card.component.ts
+++ yet-another-todo-app-frontend/src/app/components/timeline/task-card/task-card.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Task } from '../../../models/task.type';
 
 @Component({
   selector: 'yata-task-card',
   templateUrl: './task-card.component.html',
   styleUrls: ['./task-card.component.scss'],
 })
-export class TaskCardComponent implements OnInit {
-  constructor() {}
+export class TaskCardComponent {
+  @Input() task!: Task;
 
-  ngOnInit(): void {}
+  constructor() {}
 }
