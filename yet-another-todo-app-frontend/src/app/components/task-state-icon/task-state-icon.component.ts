99ce2c173941522453235f8d831494527e9f2ba2
Krzysztof Pajk <chris.araneo@gmail.com>
2023-01-06 15:27:54 +0100

    Task state icon update

--- yet-another-todo-app-frontend/src/app/components/task-state-icon/task-state-icon.component.ts
+++ yet-another-todo-app-frontend/src/app/components/task-state-icon/task-state-icon.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { TaskState } from 'src/app/models/task-state.enum';
 
 @Component({
   selector: 'yata-task-state-icon',
   templateUrl: './task-state-icon.component.html',
   styleUrls: ['./task-state-icon.component.scss'],
 })
-export class TaskStateIconComponent {}
+export class TaskStateIconComponent implements OnChanges {
+  @Input() state!: TaskState;
+  @Input() size: number = 22;
+
+  iconName: string = '';
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['state'].currentValue !== changes['state'].previousValue) {
+      this.iconName = this.mapStateToIconName(changes['state'].currentValue);
+    }
+  }
+
+  private mapStateToIconName(state: TaskState): string {
+    const mapping: Record<TaskState, string> = {
+      [TaskState.NotStarted]: 'auto_awesome',
+    };
+
+    return mapping[state];
+  }
+}
