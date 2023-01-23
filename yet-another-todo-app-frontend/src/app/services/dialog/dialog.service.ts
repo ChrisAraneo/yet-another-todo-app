4c13279d2f7bc69f4f2c8209f7d846a43c2755b1
Krzysztof Pajk <chris.araneo@gmail.com>
2023-01-23 22:00:11 +0100

    Dialog service method opening edit task modal

--- yet-another-todo-app-frontend/src/app/services/dialog/dialog.service.ts
+++ yet-another-todo-app-frontend/src/app/services/dialog/dialog.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { MatLegacyDialog as MatDialog } from '@angular/material/legacy-dialog';
+import { EditTaskModalComponent } from 'src/app/components/edit-task-modal/edit-task-modal.component';
 import { AddTaskModalComponent } from '../../components/add-task-modal/add-task-modal.component';
 
 @Injectable({
@@ -16,6 +17,14 @@ export class DialogService {
       width: this.DIALOG_WIDTH,
       height: this.DIALOG_HEIGHT,
       panelClass: AddTaskModalComponent.PANEL_CLASS,
+    });
+  }
+
+  openEditTaskModal(): void {
+    this.dialog.open(EditTaskModalComponent, {
+      width: this.DIALOG_WIDTH,
+      height: this.DIALOG_HEIGHT,
+      panelClass: EditTaskModalComponent.PANEL_CLASS,
     });
   }
 }
