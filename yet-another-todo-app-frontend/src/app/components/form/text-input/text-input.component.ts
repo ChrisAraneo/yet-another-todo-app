c842aa17ff7eab196a67c499ada111051e48fe78
Krzysztof Pajk <chris.araneo@gmail.com>
2023-01-25 23:48:36 +0100

    Added label to text input, styling

--- yet-another-todo-app-frontend/src/app/components/form/text-input/text-input.component.ts
+++ yet-another-todo-app-frontend/src/app/components/form/text-input/text-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, forwardRef } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
@@ -14,6 +14,8 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
   ],
 })
 export class TextInputComponent implements ControlValueAccessor {
+  @Input() label: string = '';
+
   value: string;
   isDisabled: boolean;
 
