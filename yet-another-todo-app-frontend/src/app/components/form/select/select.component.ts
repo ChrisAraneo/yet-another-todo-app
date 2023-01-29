90331a769fe05576300c7a567eab93ec88136b16
Krzysztof Pajk <chris.araneo@gmail.com>
2023-01-29 13:44:01 +0100

    Select component implementation and styling

--- yet-another-todo-app-frontend/src/app/components/form/select/select.component.ts
+++ yet-another-todo-app-frontend/src/app/components/form/select/select.component.ts
@@ -1,8 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+type Option = {
+  label: string;
+  value: any;
+};
 
 @Component({
   selector: 'yata-select',
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectComponent),
+      multi: true,
+    },
+  ],
 })
-export class SelectComponent {}
+export class SelectComponent implements ControlValueAccessor {
+  @Input() label: string = '';
+  @Input() options: Option[] = [];
+
+  value: any;
+  isDisabled: boolean;
+
+  changed?: (value: any) => void;
+  touched?: () => void;
+
+  constructor() {
+    this.isDisabled = false;
+  }
+
+  onChange(event: Event): void {
+    const selectedIndex: string = (<HTMLInputElement>event.target).value;
+    const selectedOption = this.options[+selectedIndex];
+
+    this.value = selectedOption.value;
+    this.changed && this.changed(selectedOption.value);
+  }
+
+  onBlur(): void {
+    this.touched && this.touched();
+  }
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.changed = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.touched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+}
