1fc3e2648cb50899c5676de72939c62fe42699dd
Krzysztof Pajk <chris.araneo@gmail.com>
2023-01-30 20:11:45 +0100

    Update task action & reducer, sorting tasks in store by creation date

--- yet-another-todo-app-frontend/src/app/store/actions/task.actions.ts
+++ yet-another-todo-app-frontend/src/app/store/actions/task.actions.ts
@@ -3,8 +3,10 @@ import { Task } from '../../models/task.model';
 
 const CREATE_TASK = 'CREATE_TASK';
 const REMOVE_TASK = 'REMOVE_TASK';
+const UPDATE_TASK = 'UPDATE_TASK';
 const SET_TASKS = 'SET_TASKS';
 
 export const createTask = createAction(CREATE_TASK, props<{ task: Task }>());
 export const removeTask = createAction(REMOVE_TASK, props<{ id: string }>());
+export const updateTask = createAction(UPDATE_TASK, props<{ task: Task }>());
 export const setTasks = createAction(SET_TASKS, props<{ tasks: Task[] }>());
--- yet-another-todo-app-frontend/src/app/store/reducers/task.reducer.ts
+++ yet-another-todo-app-frontend/src/app/store/reducers/task.reducer.ts
@@ -1,12 +1,23 @@
 import { createReducer, on } from '@ngrx/store';
 import { Task } from '../../models/task.model';
-import { createTask, removeTask, setTasks } from '../actions/task.actions';
+import { createTask, removeTask, setTasks, updateTask } from '../actions/task.actions';
 
 export const initialState: Task[] = [];
 
 export const tasksReducer = createReducer(
   initialState,
-  on(createTask, (state, { task }) => [...state, task]),
-  on(removeTask, (state, { id }) => [...state].filter((task) => task.getId() !== id)),
-  on(setTasks, (_, { tasks }) => [...tasks]),
+  on(createTask, (state, { task }) => [...state, task].sort(sortByCreationDate)),
+  on(removeTask, (state, { id }) =>
+    [...state].filter((task) => task.getId() !== id).sort(sortByCreationDate),
+  ),
+  on(updateTask, (state, { task }) =>
+    [...state]
+      .map((item) => (item.getId() === task.getId() ? task : item))
+      .sort(sortByCreationDate),
+  ),
+  on(setTasks, (_, { tasks }) => [...tasks].sort(sortByCreationDate)),
 );
+
+function sortByCreationDate(a: Task, b: Task): number {
+  return a.getCreationDate().valueOf() - b.getCreationDate().valueOf();
+}
