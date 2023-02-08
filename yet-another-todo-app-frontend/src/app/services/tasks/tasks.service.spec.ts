6d0c5aef5b691fedb23561ec90993ad0c815fef3
Krzysztof Pajk <chris.araneo@gmail.com>
2023-02-08 22:44:03 +0100

    Fixed missing imports in TasksService unit tests

--- yet-another-todo-app-frontend/src/app/services/tasks/tasks.service.spec.ts
+++ yet-another-todo-app-frontend/src/app/services/tasks/tasks.service.spec.ts
@@ -1,12 +1,19 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { StoreModule } from '@ngrx/store';
 import { TasksService } from './tasks.service';
 
 describe('TasksService', () => {
   let service: TasksService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        StoreModule.forRoot({})
+      ],
+      providers: [TasksService],
+    });
     service = TestBed.inject(TasksService);
   });
 
