41866d1aacad3ac97e90acc6bf75a29a6f2c37ed
Krzysztof Pajk <chris.araneo@gmail.com>
2023-02-08 23:05:31 +0100

    Fixed imports and providers for TimelineComponent unit tests

--- yet-another-todo-app-frontend/src/app/components/timeline/timeline.component.spec.ts
+++ yet-another-todo-app-frontend/src/app/components/timeline/timeline.component.spec.ts
@@ -1,5 +1,8 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
 import { TimelineComponent } from './timeline.component';
 
 describe('TimelineComponent', () => {
@@ -9,6 +12,16 @@ describe('TimelineComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TimelineComponent],
+      imports: [
+        MatDialogModule,
+        NoopAnimationsModule,
+        HttpClientTestingModule,
+        StoreModule.forRoot({}),
+      ],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: [] },
+      ],
     }).compileComponents();
   });
 
