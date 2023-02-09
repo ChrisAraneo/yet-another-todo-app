16919b3b0e1a7dbb6af177b104e26ebce74b6955
Krzysztof Pajk <chris.araneo@gmail.com>
2023-02-09 23:28:09 +0100

    Fixed imports and providers in AddTaskModal unit tests

--- yet-another-todo-app-frontend/src/app/components/add-task-modal/add-task-modal.component.spec.ts
+++ yet-another-todo-app-frontend/src/app/components/add-task-modal/add-task-modal.component.spec.ts
@@ -1,4 +1,9 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
 import { AddTaskModalComponent } from './add-task-modal.component';
 
 describe('AddTaskModalComponent', () => {
@@ -8,6 +13,17 @@ describe('AddTaskModalComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AddTaskModalComponent],
+      imports: [
+        MatDialogModule,
+        NoopAnimationsModule,
+        HttpClientTestingModule,
+        StoreModule.forRoot({}),
+      ],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: [] },
+        FormBuilder,
+      ],
     }).compileComponents();
   });
 
