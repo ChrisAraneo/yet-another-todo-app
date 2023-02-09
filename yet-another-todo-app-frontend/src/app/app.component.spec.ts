8b8387b78b2d791ba32eed5c85869b25ac4031fa
Krzysztof Pajk <chris.araneo@gmail.com>
2023-02-09 23:19:48 +0100

    Fixed imports and providers in AppComponent unit tests

--- yet-another-todo-app-frontend/src/app/app.component.spec.ts
+++ yet-another-todo-app-frontend/src/app/app.component.spec.ts
@@ -1,12 +1,26 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
+import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
       declarations: [AppComponent],
+      imports: [
+        RouterTestingModule,
+        MatDialogModule,
+        NoopAnimationsModule,
+        HttpClientTestingModule,
+        StoreModule.forRoot({}),
+      ],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: [] },
+      ],
     }).compileComponents();
   });
 
