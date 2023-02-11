e42b06db8157d8410a3eca8cfaba0cb299a934d8
Krzysztof Pajk <chris.araneo@gmail.com>
2023-02-11 23:09:05 +0100

    Fixed imports and providers in EditTaskModal unit tests

--- yet-another-todo-app-frontend/src/app/components/edit-task-modal/edit-task-modal.component.spec.ts
+++ yet-another-todo-app-frontend/src/app/components/edit-task-modal/edit-task-modal.component.spec.ts
@@ -1,5 +1,9 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormBuilder } from '@angular/forms';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
 import { EditTaskModalComponent } from './edit-task-modal.component';
 
 describe('EditTaskModalComponent', () => {
@@ -9,6 +13,17 @@ describe('EditTaskModalComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [EditTaskModalComponent],
+      imports: [
+        MatDialogModule,
+        NoopAnimationsModule,
+        HttpClientTestingModule,
+        StoreModule.forRoot({}),
+      ],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: [] },
+        FormBuilder,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(EditTaskModalComponent);
