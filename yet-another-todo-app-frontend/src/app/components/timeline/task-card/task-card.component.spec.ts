c0bfee721045af7a06942399580abc72ddaa1c7a
Krzysztof Pajk <chris.araneo@gmail.com>
2023-02-11 23:23:50 +0100

    Fixed TaskCard when no task is provided, unit tests fix

--- yet-another-todo-app-frontend/src/app/components/timeline/task-card/task-card.component.spec.ts
+++ yet-another-todo-app-frontend/src/app/components/timeline/task-card/task-card.component.spec.ts
@@ -1,5 +1,8 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
 import { TaskCardComponent } from './task-card.component';
 
 describe('TaskCardComponent', () => {
@@ -9,6 +12,14 @@ describe('TaskCardComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TaskCardComponent],
+      imports: [
+        MatDialogModule,
+        StoreModule.forRoot({}),
+      ],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: [] },
+      ],
     }).compileComponents();
   });
 
