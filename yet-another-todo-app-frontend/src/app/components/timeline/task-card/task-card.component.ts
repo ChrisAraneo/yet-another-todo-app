1f2815e341293e139d60eb440ab415fe7f071391
Krzysztof Pajk <chris.araneo@gmail.com>
2023-02-11 23:48:02 +0100

    Complete button logic implemented

--- yet-another-todo-app-frontend/src/app/components/timeline/task-card/task-card.component.ts
+++ yet-another-todo-app-frontend/src/app/components/timeline/task-card/task-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { DialogService } from 'src/app/services/dialog/dialog.service';
+import { TasksService } from 'src/app/services/tasks/tasks.service';
 import { Task } from '../../../models/task.model';
 
 @Component({
@@ -10,9 +11,13 @@ import { Task } from '../../../models/task.model';
 export class TaskCardComponent {
   @Input() task!: Task;
 
-  constructor(private dialogService: DialogService) {}
+  constructor(private dialogService: DialogService, private tasksService: TasksService) {}
 
   openEditTaskModal(taskId: string): void {
     this.dialogService.openEditTaskModal(taskId);
+  }
+
+  completeTask(task: Task): void {
+    this.tasksService.completeTask(task);
   }
 }
