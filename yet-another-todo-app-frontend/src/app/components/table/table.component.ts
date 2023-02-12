3508319855aa66336e75d5d3b7479c568dcf90d0
Krzysztof Pajk <chris.araneo@gmail.com>
2023-02-12 17:07:55 +0100

    TableComponent implementation

--- yet-another-todo-app-frontend/src/app/components/table/table.component.ts
+++ yet-another-todo-app-frontend/src/app/components/table/table.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { DateUtilsService } from 'src/app/services/date-utils/date-utils.service';
+import { DialogService } from 'src/app/services/dialog/dialog.service';
 import { TasksService } from 'src/app/services/tasks/tasks.service';
 import { Task } from '../../models/task.model';
 
@@ -13,18 +15,46 @@ export class TableComponent implements OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   readonly pageSizeOptions = [15, 30, 50];
-  readonly displayedColumns: string[] = ['id', 'title', 'description'];
+  readonly displayedColumns: string[] = [
+    'id',
+    'title',
+    'description',
+    'state',
+    'creationDate',
+    'startDate',
+    'endDate',
+    'actions',
+  ];
 
   data: MatTableDataSource<Task> | undefined;
 
-  constructor(private tasksService: TasksService) {}
+  constructor(
+    private tasksService: TasksService,
+    private dateUtilsService: DateUtilsService,
+    private dialogService: DialogService,
+  ) {}
 
   ngOnInit(): void {
     this.tasksService.getTasks().subscribe((tasks) => {
       this.data = new MatTableDataSource<Task>(tasks);
-      this.data.paginator = this.paginator;
+
+      if (!!this.paginator) {
+        this.data.paginator = this.paginator;
+      }
     });
   }
 
-  ngAfterViewInit() {}
+  ngAfterViewInit() {
+    if (!!this.data) {
+      this.data.paginator = this.paginator;
+    }
+  }
+
+  openEditTaskModal(taskId: string): void {
+    this.dialogService.openEditTaskModal(taskId);
+  }
+
+  formatDate(date: Date): string {
+    return this.dateUtilsService.formatDate(date, 'dd-MM-yyyy');
+  }
 }
