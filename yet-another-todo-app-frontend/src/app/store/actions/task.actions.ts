2a0f8b3cd8d24e697b1fdcad598566bddfde413c
Krzysztof Pajk <chris.araneo@gmail.com>
2023-02-14 22:33:20 +0100

    Hide task action

--- yet-another-todo-app-frontend/src/app/store/actions/task.actions.ts
+++ yet-another-todo-app-frontend/src/app/store/actions/task.actions.ts
@@ -4,9 +4,11 @@ import { Task } from '../../models/task.model';
 const CREATE_TASK = 'CREATE_TASK';
 const REMOVE_TASK = 'REMOVE_TASK';
 const UPDATE_TASK = 'UPDATE_TASK';
+const HIDE_TASK = 'HIDE_TASK';
 const SET_TASKS = 'SET_TASKS';
 
 export const createTask = createAction(CREATE_TASK, props<{ task: Task }>());
 export const removeTask = createAction(REMOVE_TASK, props<{ id: string }>());
 export const updateTask = createAction(UPDATE_TASK, props<{ task: Task }>());
+export const hideTask = createAction(HIDE_TASK, props<{ id: string }>());
 export const setTasks = createAction(SET_TASKS, props<{ tasks: Task[] }>());
