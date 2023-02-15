626d7f9bbcac8303466838773edb465c30cd4fa2
Krzysztof Pajk <chris.araneo@gmail.com>
2023-02-15 20:11:59 +0100

    Updated DeleteTaskModal unit tests

--- yet-another-todo-app-frontend/src/app/components/delete-task-modal/delete-task-modal.component.spec.ts
+++ yet-another-todo-app-frontend/src/app/components/delete-task-modal/delete-task-modal.component.spec.ts
@@ -1,4 +1,9 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
 import { DeleteTaskModalComponent } from './delete-task-modal.component';
 
 describe('DeleteTaskModalComponent', () => {
@@ -8,8 +13,21 @@ describe('DeleteTaskModalComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [DeleteTaskModalComponent],
+      imports: [
+        MatDialogModule,
+        NoopAnimationsModule,
+        HttpClientTestingModule,
+        StoreModule.forRoot({}),
+      ],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: [] },
+        FormBuilder,
+      ],
     }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(DeleteTaskModalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
