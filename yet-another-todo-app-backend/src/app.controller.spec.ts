a08908ca513386d3820c8c8f226a43f674465470
Krzysztof Pajk <chris.araneo@gmail.com>
2023-03-01 22:22:02 +0100

    Returning example tasks to GET requests

--- yet-another-todo-app-backend/src/app.controller.spec.ts
+++ yet-another-todo-app-backend/src/app.controller.spec.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
+  let stubTasksResponse: string;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -11,12 +12,15 @@ describe('AppController', () => {
       providers: [AppService],
     }).compile();
 
+    stubTasksResponse =
+      '[{"title":"Lorem ipsum dolor sit","description":"Nam consectetur tempus hendrerit. Nullam pharetra, risus eget feugiat maximus, tellus magna dignissim eros, at pulvinar nisi purus vitae lectus.","state":{"value":"IN_PROGRESS","iconName":"autorenew","color":"orange","tooltipText":"Task in progress"},"creationDate":"2023-03-01T19:43:44.738Z","id":"8e7f40b7-71a2-44fa-b112-8539c1cc068c","startDate":"2023-03-01T19:43:44.738Z"},{"title":"Curabitur ornare fringilla","description":"Proin nulla ligula, cursus vel sapien in, euismod placerat elit. Vestibulum volutpat lectus id tempor pellentesque. Vestibulum suscipit vulputate augue sit amet porttitor. Sed eget cursus tortor. Ut a enim et dolor aliquet interdum sit amet porttitor sem.","state":{"value":"IN_PROGRESS","iconName":"autorenew","color":"orange","tooltipText":"Task in progress"},"creationDate":"2023-03-02T15:45:44.738Z","id":"8e7f40b7-71a2-44fa-b112-8539c1cc068c","startDate":"2023-03-02T16:45:44.738Z"}]';
+
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('tasks', () => {
+    it('should return example tasks', () => {
+      expect(appController.getTasks()).toBe(stubTasksResponse);
     });
   });
 });
--- yet-another-todo-app-backend/test/app.e2e-spec.ts
+++ yet-another-todo-app-backend/test/app.e2e-spec.ts
@@ -5,20 +5,24 @@ import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let stubTasksResponse: string;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
+    stubTasksResponse =
+      '[{"title":"Lorem ipsum dolor sit","description":"Nam consectetur tempus hendrerit. Nullam pharetra, risus eget feugiat maximus, tellus magna dignissim eros, at pulvinar nisi purus vitae lectus.","state":{"value":"IN_PROGRESS","iconName":"autorenew","color":"orange","tooltipText":"Task in progress"},"creationDate":"2023-03-01T19:43:44.738Z","id":"8e7f40b7-71a2-44fa-b112-8539c1cc068c","startDate":"2023-03-01T19:43:44.738Z"},{"title":"Curabitur ornare fringilla","description":"Proin nulla ligula, cursus vel sapien in, euismod placerat elit. Vestibulum volutpat lectus id tempor pellentesque. Vestibulum suscipit vulputate augue sit amet porttitor. Sed eget cursus tortor. Ut a enim et dolor aliquet interdum sit amet porttitor sem.","state":{"value":"IN_PROGRESS","iconName":"autorenew","color":"orange","tooltipText":"Task in progress"},"creationDate":"2023-03-02T15:45:44.738Z","id":"8e7f40b7-71a2-44fa-b112-8539c1cc068c","startDate":"2023-03-02T16:45:44.738Z"}]';
+
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/tasks (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/tasks')
       .expect(200)
-      .expect('Hello World!');
+      .expect(stubTasksResponse);
   });
 });
