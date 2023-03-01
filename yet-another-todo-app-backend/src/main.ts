7bea4e0dd539fa8c7b271123134c702ce04236b5
Krzysztof Pajk <chris.araneo@gmail.com>
2023-03-01 22:20:48 +0100

    Starting server at port specified in env file

--- yet-another-todo-app-backend/src/main.ts
+++ yet-another-todo-app-backend/src/main.ts
@@ -1,9 +1,15 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.init();
+
+  const configService: ConfigService = app.get(ConfigService);
+
+  await app.listen(configService.get('PORT'));
 }
 
 bootstrap();
