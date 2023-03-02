349544176264aaf0645bab0d0e6ef5f9de1016a3
Krzysztof Pajk <chris.araneo@gmail.com>
2023-03-02 23:15:28 +0100

    Post endpoint, refactoring

--- yet-another-todo-app-backend/src/app.controller.ts
+++ yet-another-todo-app-backend/src/app.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get, Header } from '@nestjs/common';
+import { Controller, Get, Header, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from './models/response.type';
+import { Status } from './models/status.enum';
+import { Tasks } from './models/tasks.type';
 
 @Controller()
 export class AppController {
@@ -9,6 +12,23 @@ export class AppController {
   @Header('content-type', 'application/json')
   @Header('Access-Control-Allow-Origin', '*') // TODO Temporary... unsafe and only for testing purposes
   getTasks(): string {
-    return this.appService.getTasks();
+    const response: Tasks = {
+      status: Status.Success,
+      data: this.appService.getTasks(),
+    };
+
+    return JSON.stringify(response);
+  }
+
+  @Post('tasks')
+  @Header('content-type', 'application/json')
+  @Header('Access-Control-Allow-Origin', '*') // TODO Temporary... unsafe and only for testing purposes
+  setTasks(): string {
+    const response: Response<null> = {
+      status: Status.Success,
+      data: null,
+    };
+
+    return JSON.stringify(response);
   }
 }
