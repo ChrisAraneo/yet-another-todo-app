0fde1be85961e7435c3e398862bedc013647cb82
Krzysztof Pajk <chris.araneo@gmail.com>
2023-03-03 22:11:24 +0100

    Created Subtitle component

--- yet-another-todo-app-frontend/src/app/app.module.ts
+++ yet-another-todo-app-frontend/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { TextInputComponent } from './components/form/text-input/text-input.comp
 import { TextareaComponent } from './components/form/textarea/textarea.component';
 import { NavigationItemComponent } from './components/side-navigation/navigation-item/navigation-item.component';
 import { SideNavigationComponent } from './components/side-navigation/side-navigation.component';
+import { SubtitleComponent } from './components/subtitle/subtitle.component';
 import { SearchbarComponent } from './components/table/searchbar/searchbar.component';
 import { TableComponent } from './components/table/table.component';
 import { TaskStateIconComponent } from './components/task-state-icon/task-state-icon.component';
@@ -63,6 +64,7 @@ import { tasksReducer } from './store/reducers/task.reducer';
     DeleteTaskModalComponent,
     DatesFilterComponent,
     SearchbarComponent,
+    SubtitleComponent,
   ],
   imports: [
     StoreModule.forRoot({ tasks: tasksReducer }),
