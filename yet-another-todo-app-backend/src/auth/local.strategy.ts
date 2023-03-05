63344b5f9ec3eaa3fef0d5f64876f409c996c865
Krzysztof Pajk <chris.araneo@gmail.com>
2023-03-05 18:42:44 +0100

    Local strategy, updated Auth module imports and providers

---
+++ yet-another-todo-app-backend/src/auth/local.strategy.ts
@@ -0,0 +1,23 @@
+import { UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common/decorators/core/injectable.decorator';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { User } from 'src/models/user.type';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super();
+  }
+
+  async validate(username: string, password: string): Promise<User> {
+    const user = await this.authService.validateUser(username, password);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
