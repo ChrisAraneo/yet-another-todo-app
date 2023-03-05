eba2d082e1b0843f56d9668a2e2209c85919dc3d
Krzysztof Pajk <chris.araneo@gmail.com>
2023-03-05 18:40:08 +0100

    Validate user method

--- yet-another-todo-app-backend/src/auth/auth.service.ts
+++ yet-another-todo-app-backend/src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/models/user.type';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UsersService) {}
+
+  async validateUser(username: string, password: string): Promise<User | null> {
+    const user = await this.userService.findUser(username);
+
+    if (user && user.password === password) {
+      return user;
+    }
+
+    return null;
+  }
+}
