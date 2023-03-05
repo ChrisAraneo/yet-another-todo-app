4f728ad48b9886a52d72b0ee15c3d632c083e192
Krzysztof Pajk <chris.araneo@gmail.com>
2023-03-05 18:52:51 +0100

    Fixed import typo

--- yet-another-todo-app-backend/src/app.module.ts
+++ yet-another-todo-app-backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
@@ -11,7 +11,7 @@ import { UsersModule } from './users/users.module';
       isGlobal: true,
     }),
     UsersModule,
-    AuthService,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
