06259416cf435528da1be93004a8d45e8877415d
Krzysztof Pajk <chris.araneo@gmail.com>
2023-03-05 18:30:24 +0100

    Created Auth module, Auth service

--- yet-another-todo-app-backend/src/app.module.ts
+++ yet-another-todo-app-backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
@@ -10,6 +11,7 @@ import { UsersModule } from './users/users.module';
       isGlobal: true,
     }),
     UsersModule,
+    AuthService,
   ],
   controllers: [AppController],
   providers: [AppService],
