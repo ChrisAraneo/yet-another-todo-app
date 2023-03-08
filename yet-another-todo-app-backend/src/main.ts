a0cb7a58937aca7dd8f747751dc7db2cc30be22f
Krzysztof Pajk <chris.araneo@gmail.com>
2023-03-08 23:13:13 +0100

    CORS env var and support for enabling it

--- yet-another-todo-app-backend/src/main.ts
+++ yet-another-todo-app-backend/src/main.ts
@@ -5,6 +5,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const port: number = +process.env.PORT;
+  const enabledCors = !!process.env.ENABLED_CORS;
+
+  if (enabledCors) {
+    const origin = `${process.env.CORS_ORIGIN}`;
+
+    app.enableCors({
+      origin: origin,
+      methods: 'GET, PUT, POST, DELETE, OPTIONS',
+    });
+  }
 
   await app.listen(port);
 }
