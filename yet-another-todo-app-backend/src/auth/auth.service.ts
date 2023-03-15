30b0a63bfaafb3cba3326ef6fb6fda13972460f8
ChrisAraneo <chris.araneo@gmail.com>
2023-03-15 23:24:41 +0100

    Change imports to relative

--- yet-another-todo-app-backend/src/auth/auth.service.ts
+++ yet-another-todo-app-backend/src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { User } from 'src/models/user.type';
-import { UsersService } from 'src/users/users.service';
+import { User } from '../models/user.type';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
