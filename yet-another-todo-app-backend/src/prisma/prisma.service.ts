a139e2a22c2c4adc2d5d569571db33d387395cf2
ChrisAraneo <chris.araneo@gmail.com>
2023-03-17 23:56:18 +0100

    Implement getUser and createUser in PrismaService

--- yet-another-todo-app-backend/src/prisma/prisma.service.ts
+++ yet-another-todo-app-backend/src/prisma/prisma.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { User } from '../models/user.type';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL,
+        },
+      },
+    });
+  }
+
+  async getUser(username: string): Promise<User> {
+    return await this.user.findUnique({
+      where: {
+        username: username,
+      },
+    });
+  }
+
+  async createUser(user: User): Promise<User> {
+    return await this.user.create({
+      data: {
+        name: user.name,
+        username: user.username,
+        password: user.password,
+      },
+    });
+  }
+}
