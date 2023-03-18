081a081076484e6925114f4c358ed2032450e167
ChrisAraneo <chris.araneo@gmail.com>
2023-03-18 22:51:02 +0100

    Create Tasks module

--- yet-another-todo-app-backend/src/app.module.ts
+++ yet-another-todo-app-backend/src/app.module.ts
@@ -7,6 +7,7 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
 
 dotenv.config();
 
@@ -25,6 +26,7 @@ dotenv.config();
       global: true,
     },
     PrismaModule,
+    TasksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
