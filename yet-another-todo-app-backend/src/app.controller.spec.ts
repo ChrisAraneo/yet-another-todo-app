0433251f35473ca49cf8067bb1095cda16890c52
ChrisAraneo <chris.araneo@gmail.com>
2023-03-19 20:45:33 +0100

    Fix app controller unit test

--- yet-another-todo-app-backend/src/app.controller.spec.ts
+++ yet-another-todo-app-backend/src/app.controller.spec.ts
@@ -10,7 +10,10 @@ import { UsersService } from './users/users.service';
 
 describe('AppController', () => {
   let appController: AppController;
-  let response: object;
+  const tasks = JSON.parse(
+    `[{"creationDate": "2023-03-01T19:43:44.738Z", "description": "Nam consectetur tempus hendrerit. Nullam pharetra, risus eget feugiat maximus, tellus magna dignissim eros, at pulvinar nisi purus vitae lectus.", "id": "8e7f40b7-71a2-44fa-b112-8539c1cc068c", "startDate": "2023-03-01T19:43:44.738Z", "state": {"id": "17fc6138-53c6-41d9-b3dd-83ef2ed032ab","color": "orange", "iconName": "autorenew", "tooltipText": "Task in progress", "value": "IN_PROGRESS"}, "title": "Lorem ipsum dolor sit"}, {"creationDate": "2023-03-02T15:45:44.738Z", "description": "Proin nulla ligula, cursus vel sapien in, euismod placerat elit. Vestibulum volutpat lectus id tempor pellentesque. Vestibulum suscipit vulputate augue sit amet porttitor. Sed eget cursus tortor. Ut a enim et dolor aliquet interdum sit amet porttitor sem.", "id": "8e7f40b7-71a2-44fa-b112-8539c1cc068c", "startDate": "2023-03-02T16:45:44.738Z", "state": {"id": "17fc6138-53c6-41d9-b3dd-83ef2ed032ab","color": "orange", "iconName": "autorenew", "tooltipText": "Task in progress", "value": "IN_PROGRESS"}, "title": "Curabitur ornare fringilla"}]`,
+  );
+  const response = { data: tasks, status: 'success' };
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -40,22 +43,22 @@ describe('AppController', () => {
         },
         {
           provide: PrismaService,
-          useValue: {},
+          useValue: {
+            getTasksOfUser: jest.fn(async (_) => {
+              return tasks;
+            }),
+          },
         },
       ],
     }).compile();
-
-    response = JSON.parse(
-      '{"data": [{"creationDate": "2023-03-01T19:43:44.738Z", "description": "Nam consectetur tempus hendrerit. Nullam pharetra, risus eget feugiat maximus, tellus magna dignissim eros, at pulvinar nisi purus vitae lectus.", "id": "8e7f40b7-71a2-44fa-b112-8539c1cc068c", "startDate": "2023-03-01T19:43:44.738Z", "state": {"id": "17fc6138-53c6-41d9-b3dd-83ef2ed032ab","color": "orange", "iconName": "autorenew", "tooltipText": "Task in progress", "value": "IN_PROGRESS"}, "title": "Lorem ipsum dolor sit"}, {"creationDate": "2023-03-02T15:45:44.738Z", "description": "Proin nulla ligula, cursus vel sapien in, euismod placerat elit. Vestibulum volutpat lectus id tempor pellentesque. Vestibulum suscipit vulputate augue sit amet porttitor. Sed eget cursus tortor. Ut a enim et dolor aliquet interdum sit amet porttitor sem.", "id": "8e7f40b7-71a2-44fa-b112-8539c1cc068c", "startDate": "2023-03-02T16:45:44.738Z", "state": {"id": "17fc6138-53c6-41d9-b3dd-83ef2ed032ab","color": "orange", "iconName": "autorenew", "tooltipText": "Task in progress", "value": "IN_PROGRESS"}, "title": "Curabitur ornare fringilla"}], "status": "success"}',
-    );
 
     appController = app.get<AppController>(AppController);
   });
 
   describe('tasks', () => {
-    it('should return example tasks', () => {
+    it('should return example tasks', async () => {
       expect(
-        appController.getTasks('e42ad191-40b5-44b7-b630-17846032e70c'),
+        await appController.getTasks('e42ad191-40b5-44b7-b630-17846032e70c'),
       ).toStrictEqual(response);
     });
   });
