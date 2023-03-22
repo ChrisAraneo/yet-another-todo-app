9697a85e8718f8820fc892b053d1162a4c38e716
ChrisAraneo <chris.araneo@gmail.com>
2023-03-23 00:44:11 +0100

    Implement User service

--- yet-another-todo-app-frontend/src/app/services/user/user.service.ts
+++ yet-another-todo-app-frontend/src/app/services/user/user.service.ts
@@ -1,6 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { SignInData } from 'src/app/models/sign-in-data.model';
+import { removePassword, setPassword, setUsername } from 'src/app/store/actions/user.actions';
 
 @Injectable({
   providedIn: 'root',
 })
-export class UserService {}
+export class UserService {
+  constructor(private store: Store<{ user: SignInData }>) {}
+
+  getUser(): Observable<SignInData> {
+    return this.store.select('user');
+  }
+
+  setUser(username: string, password: string): void {
+    this.store.dispatch(setUsername({ username }));
+    this.store.dispatch(setPassword({ password }));
+  }
+
+  removePassword(): void {
+    this.store.dispatch(removePassword());
+  }
+}
