6339172e72fbafc5cd3008dad4bc912052da38f4
ChrisAraneo <chris.araneo@gmail.com>
2023-03-24 23:36:42 +0100

    Add setIsLogged action

--- yet-another-todo-app-frontend/src/app/store/actions/user.actions.ts
+++ yet-another-todo-app-frontend/src/app/store/actions/user.actions.ts
@@ -3,7 +3,9 @@ import { createAction, props } from '@ngrx/store';
 const SET_USERNAME = 'SET_USERNAME';
 const SET_PASSWORD = 'SET_PASSWORD'; // TODO Check security
 const REMOVE_PASSWORD = 'REMOVE_PASSWORD';
+const SET_IS_LOGGED = 'SET_IS_LOGGED';
 
 export const setUsername = createAction(SET_USERNAME, props<{ username: string }>());
 export const setPassword = createAction(SET_PASSWORD, props<{ password: string }>());
 export const removePassword = createAction(REMOVE_PASSWORD);
+export const setIsLogged = createAction(SET_IS_LOGGED, props<{ isLogged: boolean }>());
