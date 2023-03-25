d31a66826b7a8972c63eb4c7954ce96c8da0a6ea
ChrisAraneo <chris.araneo@gmail.com>
2023-03-25 18:07:01 +0100

    Change result type of getTaskOfUser, fetch task states during operation

--- yet-another-todo-app-backend/src/tasks/tasks.service.ts
+++ yet-another-todo-app-backend/src/tasks/tasks.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from '@prisma/client';
+import {
+  Task as TaskSchema,
+  TaskState as TaskStateSchema,
+} from '@prisma/client';
+import { Task } from '../models/tasks.type';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
@@ -7,6 +11,31 @@ export class TasksService {
   constructor(private prismaService: PrismaService) {}
 
   async getTasksOfUser(username: string): Promise<Task[] | undefined> {
-    return await this.prismaService.getTasksOfUser(username);
+    const taskStates: TaskStateSchema[] =
+      await this.prismaService.getTaskStates();
+
+    const tasks: TaskSchema[] = await this.prismaService.getTasksOfUser(
+      username,
+    );
+
+    return (tasks || []).map((item: TaskSchema) => {
+      const taskState = taskStates.find((state) => state.id === item.stateId);
+
+      return {
+        id: item.id,
+        title: item.title,
+        description: item.description,
+        state: {
+          id: taskState.id,
+          value: taskState.value,
+          iconName: taskState.iconName,
+          color: taskState.color,
+          tooltipText: taskState.tooltipText,
+        },
+        creationDate: item.creationDate.toISOString(),
+        startDate: item.startDate.toISOString(),
+        endDate: item.endDate.toISOString(),
+      };
+    });
   }
 }
