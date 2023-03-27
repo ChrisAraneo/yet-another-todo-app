57038297f30a074d0ed7cec0bcef9aebfe403ab2
ChrisAraneo <chris.araneo@gmail.com>
2023-03-27 23:42:47 +0200

    Use logic from Auth service

--- yet-another-todo-app-frontend/src/app/interceptors/token/token.interceptor.ts
+++ yet-another-todo-app-frontend/src/app/interceptors/token/token.interceptor.ts
@@ -1,54 +1,11 @@
-import {
-  HttpBackend,
-  HttpClient,
-  HttpHandler,
-  HttpInterceptor,
-  HttpRequest,
-} from '@angular/common/http';
-import { Inject, Injectable, OnDestroy } from '@angular/core';
-import {
-  BehaviorSubject,
-  catchError,
-  filter,
-  first,
-  map,
-  Observable,
-  Subscription,
-  switchMap,
-} from 'rxjs';
-import { ApiResponse, ApiResponseStatus } from 'src/app/services/api-client/api-client.types';
-import { UserService } from 'src/app/services/user/user.service';
+import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, mergeMap, Observable } from 'rxjs';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Injectable()
-export class TokenInterceptor implements HttpInterceptor, OnDestroy {
-  private readonly loginEndpoint;
-
-  private username = new BehaviorSubject<string>('');
-  private password = new BehaviorSubject<string | null>(null);
-  private token = new BehaviorSubject<string | null>(null);
-  private http: HttpClient;
-  private subscription: Subscription;
-
-  constructor(
-    @Inject('API') public api: any,
-    httpBackend: HttpBackend,
-    private userService: UserService,
-  ) {
-    this.loginEndpoint = `${this.api.origin}/login`;
-    this.http = new HttpClient(httpBackend);
-
-    this.subscription = userService
-      .getUser()
-      .pipe(filter((user) => !!user.username && !!user.password))
-      .subscribe((user) => {
-        this.username.next(user.username);
-        this.password.next(user.password);
-      });
-  }
-
-  ngOnDestroy(): void {
-    this.subscription && this.subscription.unsubscribe();
-  }
+export class TokenInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<any> {
     if (request.method === 'OPTIONS') {
@@ -58,7 +15,7 @@ export class TokenInterceptor implements HttpInterceptor, OnDestroy {
     return next
       .handle(
         request.clone({
-          headers: request.headers.append('Authorization', `Bearer ${this.token.getValue()}`),
+          headers: request.headers.append('Authorization', `Bearer ${this.authService.getToken()}`),
         }),
       )
       .pipe(
@@ -71,35 +28,17 @@ export class TokenInterceptor implements HttpInterceptor, OnDestroy {
   }
 
   handleResponseError(request: HttpRequest<unknown>, next: any): Observable<any> {
-    return this.refreshToken(this.username.getValue(), this.password.getValue()).pipe(
-      switchMap((token: string | null) => {
-        this.setTokenAndRemovePassword(token);
-
+    return this.authService.refreshToken().pipe(
+      mergeMap(() => {
         return next.handle(
           request.clone({
-            headers: request.headers.append('Authorization', `Bearer ${token}`),
+            headers: request.headers.append(
+              'Authorization',
+              `Bearer ${this.authService.getToken()}`,
+            ),
           }),
         );
       }),
     );
-  }
-
-  private refreshToken(username: string, password: string | null): Observable<string | null> {
-    // TODO Refactor into AuthService
-    return this.http.post<ApiResponse<string>>(this.loginEndpoint, { username, password }).pipe(
-      first(),
-      map((response: ApiResponse<string | null>) => {
-        if (response && response.status === ApiResponseStatus.Success) {
-          return response.data || null;
-        }
-
-        return null;
-      }),
-    );
-  }
-
-  private setTokenAndRemovePassword(token: string | null): void {
-    this.token.next(token);
-    this.userService.removePassword();
   }
 }
