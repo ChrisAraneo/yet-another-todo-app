7fd8d84839972ea4abc1f24b3fa35a5846ed8326
ChrisAraneo <chris.araneo@gmail.com>
2023-04-01 00:31:55 +0200

    Add send hide task request action

--- yet-another-todo-app-frontend/src/app/store/actions/task.actions.ts
+++ yet-another-todo-app-frontend/src/app/store/actions/task.actions.ts
@@ -3,6 +3,7 @@ import { Task } from '../../models/task.model';
 
 export const CREATE_TASK_API = 'CREATE_TASK_API';
 export const UPDATE_TASK_API = 'UPDATE_TASK_API';
+export const HIDE_TASK_API = 'HIDE_TASK_API';
 
 const CREATE_TASK = 'CREATE_TASK';
 const REMOVE_TASK = 'REMOVE_TASK';
@@ -12,6 +13,7 @@ const SET_TASKS = 'SET_TASKS';
 
 export const sendCreateTaskRequest = createAction(CREATE_TASK_API, props<{ task: Task }>());
 export const sendUpdateTaskRequest = createAction(UPDATE_TASK_API, props<{ task: Task }>());
+export const sendHideTaskRequest = createAction(HIDE_TASK_API, props<{ id: string }>());
 
 export const createTask = createAction(CREATE_TASK, props<{ task: Task }>());
 export const removeTask = createAction(REMOVE_TASK, props<{ id: string }>());
