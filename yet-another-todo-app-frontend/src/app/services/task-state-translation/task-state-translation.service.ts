09ccd97f95f54d57fa80354adbccff0c9e5fa0d3
ChrisAraneo <chris.araneo@gmail.com>
2023-04-05 23:51:40 +0200

    Implement #getTranslatedSelectOptions

--- yet-another-todo-app-frontend/src/app/services/task-state-translation/task-state-translation.service.ts
+++ yet-another-todo-app-frontend/src/app/services/task-state-translation/task-state-translation.service.ts
@@ -1,6 +1,35 @@
 import { Injectable } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import {
+  CompletedTaskState,
+  InProgressTaskState,
+  NotStartedTaskState,
+  RejectedTaskState,
+  SuspendedTaskState,
+  TaskState,
+} from 'src/app/models/task-state.model';
+import { Option } from '../../components/form/select/select.types';
 
 @Injectable({
   providedIn: 'root',
 })
-export class TaskStateTranslationService {}
+export class TaskStateTranslationService {
+  readonly states: TaskState[] = [
+    new NotStartedTaskState(),
+    new InProgressTaskState(),
+    new SuspendedTaskState(),
+    new CompletedTaskState(),
+    new RejectedTaskState(),
+  ];
+
+  constructor(private translatePipe: TranslatePipe) {}
+
+  getTranslatedSelectOptions(): Option<TaskState>[] {
+    return [
+      ...this.states.map((state) => ({
+        label: this.translatePipe.transform(state.toString()),
+        value: state,
+      })),
+    ];
+  }
+}
