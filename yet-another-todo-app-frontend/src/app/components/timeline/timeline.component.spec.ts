47c08c4d2fac18364a825d1999273e0e3c9c1193
ChrisAraneo <chris.araneo@gmail.com>
2023-04-08 14:36:57 +0200

    Add translations to timeline component, organize imports

--- yet-another-todo-app-frontend/src/app/components/timeline/timeline.component.spec.ts
+++ yet-another-todo-app-frontend/src/app/components/timeline/timeline.component.spec.ts
@@ -1,6 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { TimelineComponent } from './timeline.component';
