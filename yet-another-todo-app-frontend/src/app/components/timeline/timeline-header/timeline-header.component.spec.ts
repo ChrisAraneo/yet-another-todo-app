1808f92d5e39265517c232ba5458f4781efba33f
ChrisAraneo <chris.araneo@gmail.com>
2023-04-08 14:36:00 +0200

    Remove new lines

--- yet-another-todo-app-frontend/src/app/components/timeline/timeline-header/timeline-header.component.spec.ts
+++ yet-another-todo-app-frontend/src/app/components/timeline/timeline-header/timeline-header.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TimelineHeaderComponent } from './timeline-header.component';
 
 describe('TimelineHeaderComponent', () => {
