29a92f9c1ece5cc0dc55ed0a3eeb32343e8cc9a8
ChrisAraneo <chris.araneo@gmail.com>
2023-04-09 21:13:53 +0200

    Fix Auth service unit test

--- yet-another-todo-app-frontend/src/app/services/auth/auth.service.spec.ts
+++ yet-another-todo-app-frontend/src/app/services/auth/auth.service.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { StoreModule } from '@ngrx/store';
 import { MockProvider } from 'ng-mocks';
+import { of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { UserService } from '../user/user.service';
 import { AuthService } from './auth.service';
@@ -12,7 +13,12 @@ describe('AuthService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, StoreModule.forRoot({})],
-      providers: [{ provide: 'API', useValue: environment.api }, MockProvider(UserService, {})],
+      providers: [
+        { provide: 'API', useValue: environment.api },
+        MockProvider(UserService, {
+          getUser: () => of({ username: 'test', password: 'testpassword' }),
+        }),
+      ],
     });
     service = TestBed.inject(AuthService);
   });
