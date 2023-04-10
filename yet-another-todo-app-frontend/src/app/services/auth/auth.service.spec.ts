052f182bf4485c3b434f9cc7f6c031d0b51be9bb
ChrisAraneo <chris.araneo@gmail.com>
2023-04-10 16:58:27 +0200

    Add AuthService unit tests

--- yet-another-todo-app-frontend/src/app/services/auth/auth.service.spec.ts
+++ yet-another-todo-app-frontend/src/app/services/auth/auth.service.spec.ts
@@ -1,4 +1,4 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { StoreModule } from '@ngrx/store';
 import { MockProvider } from 'ng-mocks';
@@ -9,6 +9,7 @@ import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -17,13 +18,43 @@ describe('AuthService', () => {
         { provide: 'API', useValue: environment.api },
         MockProvider(UserService, {
           getUser: () => of({ username: 'test', password: 'testpassword' }),
+          setUser: () => undefined,
         }),
       ],
     });
     service = TestBed.inject(AuthService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('#signIn should trigger sending a login request', () => {
+    const response: any = {
+      status: 'success',
+      data: 'thisistoken',
+    };
+
+    service.signIn('lorem', 'ipsum');
+
+    const req = httpMock.expectOne(`${environment.api.origin}/login`);
+    expect(req.request.method).toBe('POST');
+    req.flush(response);
+  });
+
+  it('#getToken should return last stored token', () => {
+    const dummyToken = 'thisistoken';
+
+    service.signIn('lorem', 'ipsum');
+
+    const req = httpMock.expectOne(`${environment.api.origin}/login`);
+    expect(req.request.method).toBe('POST');
+    req.flush({
+      status: 'success',
+      data: dummyToken,
+    });
+
+    expect(service.getToken()).toBe(dummyToken);
   });
 });
