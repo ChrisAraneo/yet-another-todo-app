66647ce7f8e6dd8ef83246c605f7b5c8aee7411b
ChrisAraneo <chris.araneo@gmail.com>
2023-04-11 20:40:34 +0200

    Add #create

--- yet-another-todo-app-frontend/src/app/services/task-creator/task-creator.service.ts
+++ yet-another-todo-app-frontend/src/app/services/task-creator/task-creator.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core';
+import { TaskCreator } from 'src/app/models/task-creator.model';
+import { Task } from 'src/app/models/task.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class TaskCreatorService {}
+export class TaskCreatorService {
+  create(data: any): Task {
+    return TaskCreator.create(data);
+  }
+}
