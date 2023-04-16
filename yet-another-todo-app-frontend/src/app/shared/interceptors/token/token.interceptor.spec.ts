47e6c3960d10e364625712e455a92c9e93c67791
Krzysztof Pająk <chris.araneo@gmail.com>
2023-04-16 15:37:29 +0200

    Move interceptor into shared

--- yet-another-todo-app-frontend/src/app/shared/interceptors/token/token.interceptor.spec.ts
+++ yet-another-todo-app-frontend/src/app/shared/interceptors/token/token.interceptor.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { MockProvider } from 'ng-mocks';
 import { of } from 'rxjs';
-import { AuthService } from 'src/app/services/auth/auth.service';
+import { AuthService } from '../../services/auth/auth.service';
 import { TokenInterceptor } from './token.interceptor';
 
 describe('TokenInterceptor', () => {
