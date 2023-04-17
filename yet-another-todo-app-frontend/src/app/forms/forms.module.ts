952803db293008203b6f8781d56da754381b7756
Krzysztof Pająk <chris.araneo@gmail.com>
2023-04-17 20:08:28 +0200

    Create PasswordInputComponent

--- yet-another-todo-app-frontend/src/app/forms/forms.module.ts
+++ yet-another-todo-app-frontend/src/app/forms/forms.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { DatePickerComponent } from './components/date-picker/date-picker.component';
+import { PasswordInputComponent } from './components/password-input/password-input.component';
 import { SelectComponent } from './components/select/select.component';
 import { TextInputComponent } from './components/text-input/text-input.component';
 import { TextareaComponent } from './components/textarea/textarea.component';
 
 @NgModule({
-  declarations: [TextInputComponent, TextareaComponent, SelectComponent, DatePickerComponent],
+  declarations: [
+    TextInputComponent,
+    TextareaComponent,
+    SelectComponent,
+    DatePickerComponent,
+    PasswordInputComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   exports: [DatePickerComponent, SelectComponent, TextInputComponent, TextareaComponent],
