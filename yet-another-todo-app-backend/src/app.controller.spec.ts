8a1254d918d544441a7ba00dd7a5c68851550934
Krzysztof Pająk <chris.araneo@gmail.com>
2023-04-19 22:22:18 +0200

    Update AppController unit test by using DummyData

--- yet-another-todo-app-backend/src/app.controller.spec.ts
+++ yet-another-todo-app-backend/src/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { TaskState as TaskStateSchema } from '@prisma/client';
 import { Task } from 'src/models/tasks.type';
+import { DummyData } from 'test/dummy-data';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
@@ -13,42 +13,6 @@ import { UsersService } from './users/users.service';
 
 describe('AppController', () => {
   let appController: AppController;
-  const dummyExistingUser = {
-    id: '961d2c4d-8042-43a6-9a25-78d733094837',
-    name: 'Lorem',
-    username: 'lorem_ipsum',
-    password: 'Qwerty123/',
-  };
-  const dummyTaskStates: TaskStateSchema[] = [
-    {
-      id: '17fc6138-53c6-41d9-b3dd-83ef2ed032ab',
-      value: 'IN_PROGRESS',
-      iconName: 'autorenew',
-      color: 'orange',
-    },
-  ];
-  const dummyTasks: Task[] = [
-    {
-      creationDate: '2023-03-01T19:43:44.738Z',
-      description:
-        'Nam consectetur tempus hendrerit. Nullam pharetra, risus eget feugiat maximus, tellus magna dignissim eros, at pulvinar nisi purus vitae lectus.',
-      id: '8e7f40b7-71a2-44fa-b112-8539c1cc068c',
-      startDate: '2023-03-01T19:43:44.738Z',
-      state: dummyTaskStates[0],
-      title: 'Lorem ipsum dolor sit',
-      isHidden: false,
-    },
-    {
-      creationDate: '2023-03-02T15:45:44.738Z',
-      description:
-        'Proin nulla ligula, cursus vel sapien in, euismod placerat elit. Vestibulum volutpat lectus id tempor pellentesque. Vestibulum suscipit vulputate augue sit amet porttitor. Sed eget cursus tortor. Ut a enim et dolor aliquet interdum sit amet porttitor sem.',
-      id: '8e7f40b7-71a2-44fa-b112-8539c1cc068c',
-      startDate: '2023-03-02T16:45:44.738Z',
-      state: dummyTaskStates[0],
-      title: 'Curabitur ornare fringilla',
-      isHidden: false,
-    },
-  ];
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -64,10 +28,10 @@ describe('AppController', () => {
               async (username: string, password: string) => {
                 return new Promise<any>((resolve) => {
                   if (
-                    username === dummyExistingUser.username &&
-                    password === dummyExistingUser.password
+                    username === DummyData.user.username &&
+                    password === DummyData.user.password
                   ) {
-                    resolve({ ...dummyExistingUser });
+                    resolve({ ...DummyData.user });
                   } else {
                     resolve(null);
                   }
@@ -83,14 +47,14 @@ describe('AppController', () => {
           provide: PrismaService,
           useValue: {
             getTasksOfUser: jest.fn(async (_) => {
-              return dummyTasks;
+              return DummyData.tasks;
             }),
             getTaskStates: jest.fn(async (_) => {
-              return dummyTaskStates;
+              return DummyData.taskStates;
             }),
             createUser: jest.fn(async (user: User): Promise<User> => {
               return new Promise<any>((resolve, reject) => {
-                if (user.username === dummyExistingUser.username) {
+                if (user.username === DummyData.user.username) {
                   reject({
                     meta: {
                       target: ['username'],
@@ -107,7 +71,7 @@ describe('AppController', () => {
           provide: TasksService,
           useValue: {
             getTasksOfUser: jest.fn(async () => {
-              return dummyTasks;
+              return DummyData.tasks;
             }),
             createOrUpdateTask: jest.fn(async (_: string, task: Task) => task),
             removeTask: jest.fn(async (_: string, task: Task) => task),
@@ -120,7 +84,7 @@ describe('AppController', () => {
   });
 
   describe('signup', () => {
-    it('should return success response when user was successfuly registered', async () => {
+    it('should return success response when new user was successfuly registered', async () => {
       const newUser = {
         name: 'New user',
         username: 'new_username',
@@ -134,7 +98,7 @@ describe('AppController', () => {
     });
 
     it('should return error response when username is already taken', async () => {
-      const result = await appController.signup(dummyExistingUser);
+      const result = await appController.signup(DummyData.user);
 
       expect(result.status).toEqual(Status.Error);
       expect(result.message).toMatch(/Error: Username already exists/);
@@ -145,7 +109,7 @@ describe('AppController', () => {
     it('should return token when user provided correct credentials', async () => {
       expect(
         await appController.login({
-          user: { ...dummyExistingUser },
+          user: DummyData.user,
         }),
       ).toEqual({
         status: Status.Success,
@@ -156,31 +120,36 @@ describe('AppController', () => {
 
   describe('getTasks', () => {
     it('should return success response with tasks', async () => {
-      expect(
-        await appController.getTasks({ user: { ...dummyExistingUser } }),
-      ).toEqual({ status: Status.Success, data: dummyTasks });
+      expect(await appController.getTasks({ user: DummyData.user })).toEqual({
+        status: Status.Success,
+        data: DummyData.tasks,
+      });
     });
   });
 
   describe('createOrUpdateTask', () => {
     it('should return success response when task was successfuly created', async () => {
+      const task = DummyData.tasks[0];
+
       expect(
         await appController.createOrUpdateTask({
-          user: { ...dummyExistingUser },
-          body: dummyTasks[0],
+          user: DummyData.user,
+          body: task,
         }),
-      ).toEqual({ status: Status.Success, data: dummyTasks[0] });
+      ).toEqual({ status: Status.Success, data: task });
     });
   });
 
   describe('removeTask', () => {
     it('should return success response when task was successfuly deleted', async () => {
+      const task = DummyData.tasks[0];
+
       expect(
         await appController.removeTask({
-          user: { ...dummyExistingUser },
-          body: dummyTasks[0],
+          user: DummyData.user,
+          body: task,
         }),
-      ).toEqual({ status: Status.Success, data: dummyTasks[0] });
+      ).toEqual({ status: Status.Success, data: task });
     });
   });
 });
