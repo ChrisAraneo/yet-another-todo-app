5f38f84850734677df454ea8fffa55404967c2c8
Krzysztof Pająk <chris.araneo@gmail.com>
2023-04-20 23:14:13 +0200

    Add AuthService unit tests

--- yet-another-todo-app-backend/src/auth/auth.service.spec.ts
+++ yet-another-todo-app-backend/src/auth/auth.service.spec.ts
@@ -1,9 +1,11 @@
 import { JwtService } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
+import { DummyData } from '../../test/dummy-data';
 import { PrismaModule } from '../prisma/prisma.module';
 import { PrismaService } from '../prisma/prisma.service';
 import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/users.service';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
@@ -17,7 +19,14 @@ describe('AuthService', () => {
       providers: [
         AuthService,
         LocalStrategy,
-        JwtService,
+        {
+          provide: JwtService,
+          useValue: {
+            sign: jest.fn(async (payload: any) => {
+              return `notrealtoken${JSON.stringify(payload)}`;
+            }),
+          },
+        },
         {
           provide: JwtStrategy,
           useValue: {
@@ -34,6 +43,16 @@ describe('AuthService', () => {
           provide: PrismaService,
           useValue: {},
         },
+        {
+          provide: UsersService,
+          useValue: {
+            findUser: jest.fn(async (username: string) => {
+              return username === DummyData.user.username
+                ? DummyData.user
+                : null;
+            }),
+          },
+        },
       ],
       exports: [AuthService],
     }).compile();
@@ -43,5 +62,46 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('validateUser', () => {
+    it('should return user when provided correct credentials', async () => {
+      expect(
+        await service.validateUser(
+          DummyData.user.username,
+          DummyData.user.password,
+        ),
+      ).toEqual(DummyData.user);
+    });
+
+    it('should return null if non-existent username is provided', async () => {
+      expect(
+        await service.validateUser(
+          'non_existend_username',
+          DummyData.user.password,
+        ),
+      ).toEqual(null);
+    });
+
+    it('should return null if incorrect password is provided', async () => {
+      expect(
+        await service.validateUser(
+          DummyData.user.username,
+          'IcorrectPassw0rd123',
+        ),
+      ).toEqual(null);
+    });
+  });
+
+  describe('login', () => {
+    it('should return string token', async () => {
+      expect(
+        typeof (await service.login({
+          id: DummyData.user.id,
+          username: DummyData.user.username,
+          name: DummyData.user.name,
+        })),
+      ).toBe('string');
+    });
   });
 });
