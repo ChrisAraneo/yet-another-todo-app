748a0e6ce876b79ccb6f2c7c52943a7b5f7cc808
Krzysztof Pająk <chris.araneo@gmail.com>
2023-04-26 23:48:00 +0200

    Add sign up e2e tests, update e2e tests

--- yet-another-todo-app-backend/test/app.e2e-spec.ts
+++ yet-another-todo-app-backend/test/app.e2e-spec.ts
@@ -1,6 +1,7 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
+import { Status } from '../src/models/status.enum';
 import { AppModule } from './../src/app.module';
 
 type SignupRequest = {
@@ -11,7 +12,8 @@ type SignupRequest = {
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
-  let user: SignupRequest;
+  let existingUser: SignupRequest;
+  let newUser: SignupRequest;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -19,35 +21,41 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
-    user = {
+    existingUser = {
       name: 'E2E Test User',
       username: 'end2endTestUser',
+      password: 'pleasechangeme',
+    };
+    newUser = {
+      name: 'E2E Test New Signup User',
+      username: 'end2endNewTestUser',
       password: 'pleasechangeme',
     };
 
     await app.init();
 
-    await request(app.getHttpServer()).post('/signup').send(user);
+    await request(app.getHttpServer()).post('/signup').send(existingUser);
   });
 
   afterEach(async () => {
-    await request(app.getHttpServer()).delete('/user').send(user);
-    await request(app.getHttpServer()).delete('/user').send({
-      name: 'E2E Test New Signup User',
-      username: 'end2endNewTestUser',
-      password: 'pleasechangeme',
-    });
+    await request(app.getHttpServer()).delete('/user').send(existingUser);
+    await request(app.getHttpServer()).delete('/user').send(newUser);
   });
 
   describe('Login', () => {
     it('should login and receive valid token', () => {
-      return request(app.getHttpServer()).post('/login').send(user).expect(201);
+      return request(app.getHttpServer())
+        .post('/login')
+        .send(existingUser)
+        .expect((res) => {
+          expect(res.body.status).toBe(Status.Success);
+        });
     });
 
     it('should reject login', () => {
       return request(app.getHttpServer())
         .post('/login')
-        .send({ ...user, password: `${user.password}#` })
+        .send({ ...existingUser, password: `${existingUser.password}#` })
         .expect(401);
     });
   });
@@ -61,7 +69,20 @@ describe('AppController (e2e)', () => {
           username: 'end2endNewTestUser',
           password: 'pleasechangeme',
         })
-        .expect(201);
+        .expect((res) => {
+          expect(res.body.status).toBe(Status.Success);
+        });
+    });
+
+    it('should return error when registering  new user and receive success response', async () => {
+      await request(app.getHttpServer()).post('/signup').send(newUser);
+
+      await request(app.getHttpServer())
+        .post('/signup')
+        .send(newUser)
+        .expect((res) => {
+          expect(res.body.status).toBe(Status.Error);
+        });
     });
   });
 });
