59dee3c88dd3184e1bc81c736232362b58e6e11b
Krzysztof Pająk <chris.araneo@gmail.com>
2023-05-25 21:32:25 +0200

    Add basic structure and styles to export tasks modal

--- yet-another-todo-app-frontend/src/app/modals/components/export-tasks-modal/export-tasks-modal.component.ts
+++ yet-another-todo-app-frontend/src/app/modals/components/export-tasks-modal/export-tasks-modal.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'yata-export-tasks-modal',
   templateUrl: './export-tasks-modal.component.html',
   styleUrls: ['./export-tasks-modal.component.scss'],
 })
-export class ExportTasksModalComponent {}
+export class ExportTasksModalComponent {
+  static readonly PANEL_CLASS = 'export-tasks-modal';
+
+  constructor(public dialogRef: MatDialogRef<ExportTasksModalComponent>) {}
+}
