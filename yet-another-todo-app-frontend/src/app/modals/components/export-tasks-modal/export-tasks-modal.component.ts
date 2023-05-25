65f7f7fa41b19ae403ecb038d369d31ed6e747b4
Krzysztof Pająk <chris.araneo@gmail.com>
2023-05-25 22:29:29 +0200

    Implement #export

--- yet-another-todo-app-frontend/src/app/modals/components/export-tasks-modal/export-tasks-modal.component.ts
+++ yet-another-todo-app-frontend/src/app/modals/components/export-tasks-modal/export-tasks-modal.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { TasksService } from 'src/app/shared/services/tasks/tasks.service';
+import { ZipTasksService } from 'src/app/shared/services/zip-tasks/zip-tasks.service';
+import { Task } from '../../../shared/models/task.model';
 
 @Component({
   selector: 'yata-export-tasks-modal',
@@ -9,5 +13,27 @@ import { MatDialogRef } from '@angular/material/dialog';
 export class ExportTasksModalComponent {
   static readonly PANEL_CLASS = 'export-tasks-modal';
 
-  constructor(public dialogRef: MatDialogRef<ExportTasksModalComponent>) {}
+  tasks!: Observable<Task[]>;
+  isLoading: boolean = false;
+
+  constructor(
+    public dialogRef: MatDialogRef<ExportTasksModalComponent>,
+    private tasksService: TasksService,
+    private zipTasksService: ZipTasksService,
+  ) {
+    this.initializeTasksObservable();
+  }
+
+  export(tasks: Task[]): void {
+    this.isLoading = true;
+
+    this.zipTasksService.zip(tasks, 'TODO Add password').then(() => {
+      this.isLoading = false;
+      this.dialogRef.close();
+    });
+  }
+
+  private initializeTasksObservable(): void {
+    this.tasks = this.tasksService.getTasks();
+  }
 }
