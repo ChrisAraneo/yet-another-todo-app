34a07853bcdd48fed845824822316c9486ae7488
Krzysztof Pająk <chris.araneo@gmail.com>
2023-05-25 22:28:52 +0200

    Implement ZipTasksService

--- yet-another-todo-app-frontend/src/app/shared/services/zip-tasks/zip-tasks.service.ts
+++ yet-another-todo-app-frontend/src/app/shared/services/zip-tasks/zip-tasks.service.ts
@@ -1,6 +1,44 @@
 import { Injectable } from '@angular/core';
+import CryptoJS from 'crypto-js';
+import FileSaver from 'file-saver';
+import JSZip from 'jszip';
+import { Task } from '../../models/task.model';
+import { DateUtilsService } from '../date-utils/date-utils.service';
+
+// TODO Move to separate file
+type FileContent = {
+  creationDate: Date;
+  tasks: Task[];
+};
 
 @Injectable({
   providedIn: 'root',
 })
-export class ZipTasksService {}
+export class ZipTasksService {
+  constructor(private dateUtilsService: DateUtilsService) {}
+
+  async zip(tasks: Task[], password: string): Promise<any> {
+    const zip = new JSZip();
+
+    const creationDate = new Date();
+
+    const fileContent: FileContent = {
+      creationDate,
+      tasks,
+    };
+
+    zip.file('tasks.txt', this.encrypt(JSON.stringify(fileContent), password));
+
+    return zip.generateAsync({ type: 'blob' }).then((blob) => {
+      FileSaver.saveAs(
+        blob,
+        `yata-tasks-${this.dateUtilsService.formatDate(creationDate, 'yyyy-MM-dd-kkmmss')}.zip`,
+      );
+    });
+  }
+
+  // TODO Extract to new service
+  private encrypt(text: string, password: string): string {
+    return CryptoJS.AES.encrypt(text, password).toString();
+  }
+}
