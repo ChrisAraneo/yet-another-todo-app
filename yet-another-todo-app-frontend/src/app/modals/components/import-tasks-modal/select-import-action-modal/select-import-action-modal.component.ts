95f96b87021a97812429416afa5418a825d69bed
Krzysztof Pająk <chris.araneo@gmail.com>
2023-05-30 20:51:24 +0200

    Add radio buttons with actions to modal, update styles and component logic

--- yet-another-todo-app-frontend/src/app/modals/components/import-tasks-modal/select-import-action-modal/select-import-action-modal.component.ts
+++ yet-another-todo-app-frontend/src/app/modals/components/import-tasks-modal/select-import-action-modal/select-import-action-modal.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ImportAction } from './select-import-action-modal.types';
 
 @Component({
   selector: 'yata-select-import-action-modal',
   templateUrl: './select-import-action-modal.component.html',
   styleUrls: ['./select-import-action-modal.component.scss'],
 })
-export class SelectImportActionModalComponent {}
+export class SelectImportActionModalComponent {
+  static readonly PANEL_CLASS = 'select-import-action-modal';
+
+  readonly actions: ImportAction[] = Object.values(ImportAction);
+
+  action: ImportAction = ImportAction.AddNewAndUpdateExisting;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dialogRef: MatDialogRef<SelectImportActionModalComponent>,
+  ) {}
+
+  submit(): void {
+    // TODO
+    alert('NOT IMPLEMENTED');
+    //this.dialogRef.close();
+  }
+}
