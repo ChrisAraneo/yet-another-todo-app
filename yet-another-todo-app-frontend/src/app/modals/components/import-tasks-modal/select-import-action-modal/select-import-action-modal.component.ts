c73403899a9dfedce589534568ec3fbcec401700
Krzysztof Pająk <chris.araneo@gmail.com>
2023-06-03 19:57:26 +0200

    Implement submit in SelectImportActionModal

--- yet-another-todo-app-frontend/src/app/modals/components/import-tasks-modal/select-import-action-modal/select-import-action-modal.component.ts
+++ yet-another-todo-app-frontend/src/app/modals/components/import-tasks-modal/select-import-action-modal/select-import-action-modal.component.ts
@@ -1,5 +1,8 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnDestroy } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Subscription } from 'rxjs';
+import { TasksService } from 'src/app/shared/services/tasks/tasks.service';
+import { Task } from '../../../../shared/models/task.model';
 import { ImportAction } from './select-import-action-modal.types';
 
 @Component({
@@ -7,21 +10,30 @@ import { ImportAction } from './select-import-action-modal.types';
   templateUrl: './select-import-action-modal.component.html',
   styleUrls: ['./select-import-action-modal.component.scss'],
 })
-export class SelectImportActionModalComponent {
+export class SelectImportActionModalComponent implements OnDestroy {
   static readonly PANEL_CLASS = 'select-import-action-modal';
 
   readonly actions: ImportAction[] = Object.values(ImportAction);
 
   action: ImportAction = ImportAction.AddNewAndUpdateExisting;
 
+  private subscription?: Subscription;
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     public dialogRef: MatDialogRef<SelectImportActionModalComponent>,
+    private tasksService: TasksService,
   ) {}
 
+  ngOnDestroy(): void {
+    this.subscription && this.subscription.unsubscribe();
+  }
+
   submit(): void {
-    // TODO
-    alert('NOT IMPLEMENTED');
-    //this.dialogRef.close();
+    this.subscription = this.tasksService
+      .importTasks(this.data as Task[], this.action)
+      .subscribe(() => {
+        this.dialogRef.close();
+      });
   }
 }
