fa4908c78c30f6e9497875a8b4b641a4d8b92a00
Krzysztof Pająk <chris.araneo@gmail.com>
2023-06-08 14:26:29 +0200

    Create ErrorTooltipComponent

--- yet-another-todo-app-frontend/src/app/forms/directives/directives.module.ts
+++ yet-another-todo-app-frontend/src/app/forms/directives/directives.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
+import { ErrorTooltipComponent } from './error-tooltip/components/error-tooltip/error-tooltip.component';
 import { ErrorTooltipDirective } from './error-tooltip/error-tooltip.directive';
 
 @NgModule({
-  declarations: [ErrorTooltipDirective],
+  declarations: [ErrorTooltipDirective, ErrorTooltipComponent],
   imports: [],
   providers: [],
   exports: [ErrorTooltipDirective],
---
+++ yet-another-todo-app-frontend/src/app/forms/directives/error-tooltip/components/error-tooltip/error-tooltip.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'yata-error-tooltip',
+  templateUrl: './error-tooltip.component.html',
+  styleUrls: ['./error-tooltip.component.scss'],
+})
+export class ErrorTooltipComponent {}
