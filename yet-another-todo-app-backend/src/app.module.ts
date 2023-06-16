6728506eba7411d0e33c4290162784b2f2c9d602
Krzysztof Pająk <chris.araneo@gmail.com>
2023-06-16 23:05:51 +0200

    Remove JWT module options initialized on start

--- yet-another-todo-app-backend/src/app.module.ts
+++ yet-another-todo-app-backend/src/app.module.ts
@@ -6,8 +6,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
 
 dotenv.config();
 
@@ -19,10 +19,7 @@ dotenv.config();
     UsersModule,
     AuthModule,
     {
-      ...JwtModule.register({
-        secret: process.env.JWT_SECRET,
-        signOptions: { expiresIn: '120s' },
-      }),
+      ...JwtModule.register({}),
       global: true,
     },
     PrismaModule,
