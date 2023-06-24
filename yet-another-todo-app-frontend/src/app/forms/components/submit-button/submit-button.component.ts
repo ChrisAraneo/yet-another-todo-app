728225a55291b49c0e4e19d875e36a65399e91b9
Krzysztof Pająk <chris.araneo@gmail.com>
2023-06-24 17:06:08 +0200

    Add template and base logic to submit button

--- yet-another-todo-app-frontend/src/app/forms/components/submit-button/submit-button.component.ts
+++ yet-another-todo-app-frontend/src/app/forms/components/submit-button/submit-button.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+const NOOP = async (): Promise<void> => {
+  return;
+};
 
 @Component({
   selector: 'yata-submit-button',
   templateUrl: './submit-button.component.html',
   styleUrls: ['./submit-button.component.scss'],
 })
-export class SubmitButtonComponent {}
+export class SubmitButtonComponent {
+  @Input() invalid: boolean = false;
+  @Input() pending: boolean = false;
+  @Input() submit: () => Promise<void> = NOOP;
+
+  isLoading: boolean = false;
+
+  onButtonClick(): void {
+    this.isLoading = true;
+
+    this.submit()
+      .then(() => {
+        this.isLoading = false;
+      })
+      .catch((error: any) => {
+        this.isLoading = false;
+        console.error(error);
+      });
+  }
+}
