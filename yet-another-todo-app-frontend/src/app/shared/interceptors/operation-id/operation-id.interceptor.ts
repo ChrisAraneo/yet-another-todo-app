7b8c5f0405987ddbad919ee0a761eb834e217889
Krzysztof Pająk <chris.araneo@gmail.com>
2023-06-26 20:24:31 +0200

    Implement OperationIdInterceptor

--- yet-another-todo-app-frontend/src/app/shared/interceptors/operation-id/operation-id.interceptor.ts
+++ yet-another-todo-app-frontend/src/app/shared/interceptors/operation-id/operation-id.interceptor.ts
@@ -1,10 +1,16 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { v4 as uuidv4 } from 'uuid';
+import { OperationIdHeaderName } from './operation-id.types';
 
 @Injectable()
 export class OperationIdInterceptor implements HttpInterceptor {
-  intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<any>> {
-    throw new Error('Method not implemented.');
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(
+      request.clone({
+        headers: request.headers.append(OperationIdHeaderName, uuidv4()),
+      }),
+    );
   }
 }
