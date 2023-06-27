2a52b11c78f15c8e90766a8e3c5402b38f4226ff
Krzysztof Pająk <chris.araneo@gmail.com>
2023-06-28 00:08:40 +0200

    Refactor, use type

--- yet-another-todo-app-frontend/src/app/shared/store/actions/http-log.actions.ts
+++ yet-another-todo-app-frontend/src/app/shared/store/actions/http-log.actions.ts
@@ -1,5 +1,5 @@
 import { createAction, props } from '@ngrx/store';
-import { HttpLogType } from '../../models/http-log-type.enum';
+import { HttpLogItem } from '../../models/http-log-item.type';
 
 export const POST_SIGNUP_HTTP_LOG = 'POST_SIGNUP_HTTP_LOG';
 export const POST_LOGIN_HTTP_LOG = 'POST_LOGIN_HTTP_LOG';
@@ -10,35 +10,11 @@ export const POST_TASK_HTTP_LOG = 'POST_TASK_HTTP_LOG';
 export const DELETE_TASK_HTTP_LOG = 'DELETE_TASK_HTTP_LOG';
 export const DELETE_USER_HTTP_LOG = 'DELETE_USER_HTTP_LOG';
 
-export const pushToPostSignupHttpLog = createAction(
-  POST_SIGNUP_HTTP_LOG,
-  props<{ id: string; logType: HttpLogType; data: any; creationDate: Date }>(),
-);
-export const pushToPostLoginHttpLog = createAction(
-  POST_LOGIN_HTTP_LOG,
-  props<{ id: string; logType: HttpLogType; data: any; creationDate: Date }>(),
-);
-export const pushToPostRefreshHttpLog = createAction(
-  POST_REFRESH_HTTP_LOG,
-  props<{ id: string; logType: HttpLogType; data: any; creationDate: Date }>(),
-);
-export const pushToGetTasksHttpLog = createAction(
-  GET_TASKS_HTTP_LOG,
-  props<{ id: string; logType: HttpLogType; data: any; creationDate: Date }>(),
-);
-export const pushToPostTasksHttpLog = createAction(
-  POST_TASKS_HTTP_LOG,
-  props<{ id: string; logType: HttpLogType; data: any; creationDate: Date }>(),
-);
-export const pushToPostTaskHttpLog = createAction(
-  POST_TASK_HTTP_LOG,
-  props<{ id: string; logType: HttpLogType; data: any; creationDate: Date }>(),
-);
-export const pushToDeleteTaskHttpLog = createAction(
-  DELETE_TASK_HTTP_LOG,
-  props<{ id: string; logType: HttpLogType; data: any; creationDate: Date }>(),
-);
-export const pushToDeleteUserHttpLog = createAction(
-  DELETE_USER_HTTP_LOG,
-  props<{ id: string; logType: HttpLogType; data: any; creationDate: Date }>(),
-);
+export const pushToPostSignupHttpLog = createAction(POST_SIGNUP_HTTP_LOG, props<HttpLogItem>());
+export const pushToPostLoginHttpLog = createAction(POST_LOGIN_HTTP_LOG, props<HttpLogItem>());
+export const pushToPostRefreshHttpLog = createAction(POST_REFRESH_HTTP_LOG, props<HttpLogItem>());
+export const pushToGetTasksHttpLog = createAction(GET_TASKS_HTTP_LOG, props<HttpLogItem>());
+export const pushToPostTasksHttpLog = createAction(POST_TASKS_HTTP_LOG, props<HttpLogItem>());
+export const pushToPostTaskHttpLog = createAction(POST_TASK_HTTP_LOG, props<HttpLogItem>());
+export const pushToDeleteTaskHttpLog = createAction(DELETE_TASK_HTTP_LOG, props<HttpLogItem>());
+export const pushToDeleteUserHttpLog = createAction(DELETE_USER_HTTP_LOG, props<HttpLogItem>());
