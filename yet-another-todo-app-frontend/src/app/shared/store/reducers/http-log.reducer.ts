c7f12374b900c1c24de553eef923149fab6b3e24
Krzysztof Pająk <chris.araneo@gmail.com>
2023-06-28 21:09:46 +0200

    Create and implement HttpLogReducer

---
+++ yet-another-todo-app-frontend/src/app/shared/store/reducers/http-log.reducer.ts
@@ -0,0 +1,73 @@
+import { createReducer, on } from '@ngrx/store';
+import {
+  pushToDeleteTaskHttpLog,
+  pushToDeleteUserHttpLog,
+  pushToGetTasksHttpLog,
+  pushToPostLoginHttpLog,
+  pushToPostRefreshHttpLog,
+  pushToPostSignupHttpLog,
+  pushToPostTaskHttpLog,
+  pushToPostTasksHttpLog,
+} from '../actions/http-log.actions';
+
+type HttpLogState = {
+  // TODO Move the type to a separate file and improve the type by specifying the keys
+  get: {
+    tasks: any[];
+  };
+  post: {
+    signUp: any[];
+    login: any[];
+    refresh: any[];
+    tasks: any[];
+    task: any[];
+  };
+  delete: {
+    task: any[];
+    user: any[];
+  };
+};
+
+export const initialState: HttpLogState = {
+  get: {
+    tasks: [],
+  },
+  post: {
+    signUp: [],
+    login: [],
+    refresh: [],
+    tasks: [],
+    task: [],
+  },
+  delete: {
+    task: [],
+    user: [],
+  },
+};
+
+export const httpLogReducer = createReducer(
+  initialState,
+  on(pushToPostSignupHttpLog, (state, item) => updateHttpLogState(state, 'post', 'signUp', item)),
+  on(pushToPostLoginHttpLog, (state, item) => updateHttpLogState(state, 'post', 'login', item)),
+  on(pushToPostRefreshHttpLog, (state, item) => updateHttpLogState(state, 'post', 'refresh', item)),
+  on(pushToGetTasksHttpLog, (state, item) => updateHttpLogState(state, 'get', 'tasks', item)),
+  on(pushToPostTasksHttpLog, (state, item) => updateHttpLogState(state, 'post', 'tasks', item)),
+  on(pushToPostTaskHttpLog, (state, item) => updateHttpLogState(state, 'post', 'task', item)),
+  on(pushToDeleteTaskHttpLog, (state, item) => updateHttpLogState(state, 'delete', 'task', item)),
+  on(pushToDeleteUserHttpLog, (state, item) => updateHttpLogState(state, 'delete', 'user', item)),
+);
+
+function updateHttpLogState(
+  currentState: HttpLogState,
+  method: string,
+  endpointName: string,
+  item: any,
+): HttpLogState {
+  return {
+    ...currentState,
+    [method]: {
+      ...(currentState as any)[method],
+      [endpointName]: [...(currentState as any)[method][endpointName], item],
+    },
+  } as HttpLogState;
+}
