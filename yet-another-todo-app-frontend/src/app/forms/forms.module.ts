ad76ba77f42984a7fd96ffd5294b7533cd13ba23
Krzysztof Pająk <chris.araneo@gmail.com>
2023-07-02 22:19:49 +0200

    Update SubmitButton, add spinner, styling

--- yet-another-todo-app-frontend/src/app/forms/forms.module.ts
+++ yet-another-todo-app-frontend/src/app/forms/forms.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { BrowserModule } from '@angular/platform-browser';
 import { DatePickerComponent } from './components/date-picker/date-picker.component';
 import { FileInputComponent } from './components/file-input/file-input.component';
@@ -20,7 +21,7 @@ import { DirectivesModule } from './directives/directives.module';
     PasswordInputComponent,
     FileInputComponent,
   ],
-  imports: [BrowserModule, MatButtonModule, DirectivesModule],
+  imports: [BrowserModule, MatButtonModule, DirectivesModule, MatProgressSpinnerModule],
   providers: [],
   exports: [
     SubmitButtonComponent,
