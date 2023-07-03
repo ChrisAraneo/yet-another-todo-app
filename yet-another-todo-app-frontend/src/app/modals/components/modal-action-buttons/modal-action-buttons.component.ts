fb7f8bad2996a5c944ba86439ea467f62a328a73
Krzysztof Pająk <chris.araneo@gmail.com>
2023-07-03 22:28:48 +0200

    Implement ModalActionButtons component

--- yet-another-todo-app-frontend/src/app/modals/components/modal-action-buttons/modal-action-buttons.component.ts
+++ yet-another-todo-app-frontend/src/app/modals/components/modal-action-buttons/modal-action-buttons.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+const NOOP = async (): Promise<void> => {
+  return;
+};
 
 @Component({
   selector: 'yata-modal-action-buttons',
   templateUrl: './modal-action-buttons.component.html',
   styleUrls: ['./modal-action-buttons.component.scss'],
 })
-export class ModalActionButtonsComponent {}
+export class ModalActionButtonsComponent {
+  @Input() form?: FormGroup<any>;
+  @Input() submit: () => Promise<void> = NOOP;
+  @Input() cancel: (() => Promise<void>) | (() => void) = NOOP;
+  @Input() cancelLabel: string = '';
+  @Input() submitLabel: string = '';
+}
