8c427894dccb66d13ae79f175eaf43a96ee71b75
Krzysztof Pająk <chris.araneo@gmail.com>
2023-07-15 22:09:35 +0200

    Fix logging interceptor unit test

--- yet-another-todo-app-frontend/src/app/shared/interceptors/logging/logging.interceptor.spec.ts
+++ yet-another-todo-app-frontend/src/app/shared/interceptors/logging/logging.interceptor.spec.ts
@@ -1,11 +1,26 @@
 import { TestBed } from '@angular/core/testing';
+import { Store } from '@ngrx/store';
+import { MockProvider } from 'ng-mocks';
+import { of } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { LoggingInterceptor } from './logging.interceptor';
 
 describe('LoggingInterceptor', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
       imports: [],
-      providers: [LoggingInterceptor],
+      providers: [
+        { provide: 'API', useValue: environment.api },
+        LoggingInterceptor,
+        MockProvider(Store, {
+          select: (key: any) => {
+            if (key === 'httpLog') {
+              return of([]);
+            }
+            return of(undefined);
+          },
+        }),
+      ],
     }),
   );
 
