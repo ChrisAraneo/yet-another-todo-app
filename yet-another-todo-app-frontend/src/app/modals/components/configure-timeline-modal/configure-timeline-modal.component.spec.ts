5c9deb8ab5646caa4a3ebb27839523345b987c31
Krzysztof Pająk <chris.araneo@gmail.com>
2023-07-18 21:26:48 +0200

    Update ConfigureTimelineModal unit test

--- yet-another-todo-app-frontend/src/app/modals/components/configure-timeline-modal/configure-timeline-modal.component.spec.ts
+++ yet-another-todo-app-frontend/src/app/modals/components/configure-timeline-modal/configure-timeline-modal.component.spec.ts
@@ -1,4 +1,11 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { TranslatePipe } from '@ngx-translate/core';
+import { MockPipe } from 'ng-mocks';
 import { ConfigureTimelineModalComponent } from './configure-timeline-modal.component';
 
 describe('ConfigureTimelineModalComponent', () => {
@@ -7,9 +14,22 @@ describe('ConfigureTimelineModalComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ConfigureTimelineModalComponent],
+      declarations: [ConfigureTimelineModalComponent, MockPipe(TranslatePipe)],
+      imports: [
+        MatDialogModule,
+        NoopAnimationsModule,
+        HttpClientTestingModule,
+        StoreModule.forRoot({}),
+      ],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: [] },
+        FormBuilder,
+      ],
     }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(ConfigureTimelineModalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
