00289518ab3a5244b79784aea000ad1d731c9353
Krzysztof Pająk <chris.araneo@gmail.com>
2023-07-19 20:16:07 +0200

    Create and implement basic view config reducer

---
+++ yet-another-todo-app-frontend/src/app/shared/store/reducers/configuration.reducer.ts
@@ -0,0 +1,41 @@
+import { createReducer, on } from '@ngrx/store';
+import { DateUtilsService } from '../../services/date-utils/date-utils.service';
+import { setTimelineEndDate, setTimelineStartDate } from '../actions/configuration.actions';
+
+type ViewConfiguration = {
+  timeline: {
+    startDate: Date;
+    endDate: Date;
+  };
+};
+
+export const initialState: ViewConfiguration = {
+  timeline: {
+    startDate: getInitialTimelineStartDate(),
+    endDate: getInitialTimelineEndDate(),
+  },
+};
+
+export const viewConfigurationReducer = createReducer(
+  initialState,
+  on(setTimelineStartDate, (state, { startDate }) => ({
+    ...state,
+    timeline: { ...state.timeline, startDate },
+  })),
+  on(setTimelineEndDate, (state, { endDate }) => ({
+    ...state,
+    timeline: { ...state.timeline, endDate },
+  })),
+);
+
+function getInitialTimelineStartDate(): Date {
+  const today = new Date();
+
+  return new DateUtilsService().getFirstDayOfTheMonth(today);
+}
+
+function getInitialTimelineEndDate(): Date {
+  const today = new Date();
+
+  return new DateUtilsService().getLastDayOfTheMonth(today);
+}
