e8d2a5de4fccae2ef6857c6fb315f62c0b60714b
Krzysztof Pająk <chris.araneo@gmail.com>
2023-07-19 20:27:02 +0200

    Implement #changeTimelineStartDate and #changeTimelineEndDate, update unit test

--- yet-another-todo-app-frontend/src/app/shared/services/view-configuration/view-configuration.service.spec.ts
+++ yet-another-todo-app-frontend/src/app/shared/services/view-configuration/view-configuration.service.spec.ts
@@ -1,12 +1,35 @@
 import { TestBed } from '@angular/core/testing';
+import { Store } from '@ngrx/store';
+import { MockProvider } from 'ng-mocks';
+import { of } from 'rxjs';
 import { ViewConfigurationService } from './view-configuration.service';
 
+// TODO Unit tests for #setTimelineStartDate and #setTimelineEndDate
 describe('ViewConfigurationService', () => {
   let service: ViewConfigurationService;
+  let store: Store;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [],
+      providers: [
+        MockProvider(Store, {
+          select: (key: any) => {
+            if (key === 'viewConfiguration') {
+              return of({
+                timeline: {
+                  startDate: new Date(),
+                  endDate: new Date(),
+                },
+              });
+            }
+            return of(undefined);
+          },
+        }),
+      ],
+    });
     service = TestBed.inject(ViewConfigurationService);
+    store = service.store;
   });
 
   it('should be created', () => {
--- yet-another-todo-app-frontend/src/app/shared/services/view-configuration/view-configuration.service.ts
+++ yet-another-todo-app-frontend/src/app/shared/services/view-configuration/view-configuration.service.ts
@@ -1,6 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import {
+  setTimelineEndDate,
+  setTimelineStartDate,
+} from '../../store/actions/configuration.actions';
+import { ViewConfiguration } from '../../store/reducers/configuration.reducer';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ViewConfigurationService {}
+export class ViewConfigurationService {
+  constructor(public store: Store<{ viewConfigurations: ViewConfiguration }>) {}
+
+  changeTimelineStartDate(date: Date): void {
+    this.store.dispatch(setTimelineStartDate({ startDate: date }));
+  }
+
+  changeTimelineEndDate(date: Date): void {
+    this.store.dispatch(setTimelineEndDate({ endDate: date }));
+  }
+}
