612c784bf7c89f8163b7610f2c3ad58ecd5019b0
Krzysztof Pająk <chris.araneo@gmail.com>
2023-07-25 19:37:34 +0200

    Implement basic structure of ConfigureTableModal, add styles, update unit test, add translations

--- yet-another-todo-app-frontend/src/app/modals/components/configure-table-modal/configure-table-modal.component.spec.ts
+++ yet-another-todo-app-frontend/src/app/modals/components/configure-table-modal/configure-table-modal.component.spec.ts
@@ -1,4 +1,11 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { TranslatePipe } from '@ngx-translate/core';
+import { MockPipe } from 'ng-mocks';
 import { ConfigureTableModalComponent } from './configure-table-modal.component';
 
 describe('ConfigureTableModalComponent', () => {
@@ -7,9 +14,22 @@ describe('ConfigureTableModalComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ConfigureTableModalComponent],
+      declarations: [ConfigureTableModalComponent, MockPipe(TranslatePipe)],
+      imports: [
+        MatDialogModule,
+        NoopAnimationsModule,
+        HttpClientTestingModule,
+        StoreModule.forRoot({}),
+      ],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        FormBuilder,
+      ],
     }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(ConfigureTableModalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- yet-another-todo-app-frontend/src/app/modals/components/configure-table-modal/configure-table-modal.component.ts
+++ yet-another-todo-app-frontend/src/app/modals/components/configure-table-modal/configure-table-modal.component.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'yata-configure-table-modal',
   templateUrl: './configure-table-modal.component.html',
   styleUrls: ['./configure-table-modal.component.scss'],
 })
-export class ConfigureTableModalComponent {}
+export class ConfigureTableModalComponent {
+  static readonly PANEL_CLASS = 'configure-table-modal';
+
+  form?: FormGroup<any>; // TODO Specify proper type
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dialogRef: MatDialogRef<ConfigureTableModalComponent>,
+    private formBuilder: FormBuilder,
+  ) {
+    this.initializeForm();
+  }
+
+  submit: () => Promise<void> = async () => {
+    if (this.form === undefined || this.form?.invalid) {
+      return;
+    }
+
+    this.dialogRef.close();
+  };
+
+  cancel: () => void = () => {
+    this.dialogRef.close();
+  };
+
+  private initializeForm(): void {
+    this.form = this.formBuilder.group<any>({});
+  }
+}
