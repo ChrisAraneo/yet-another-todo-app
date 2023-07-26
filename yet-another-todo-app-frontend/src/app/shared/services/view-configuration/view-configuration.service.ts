dad27f797742c372a958400f1ec97f6618dbb904
Krzysztof Pająk <chris.araneo@gmail.com>
2023-07-26 20:38:21 +0200

    Add #getTableConfiguration and #changeTableSorting

--- yet-another-todo-app-frontend/src/app/shared/services/view-configuration/view-configuration.service.ts
+++ yet-another-todo-app-frontend/src/app/shared/services/view-configuration/view-configuration.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
+import { MatSortable } from '@angular/material/sort';
 import { Store } from '@ngrx/store';
 import { BehaviorSubject, Observable, Subscription, map } from 'rxjs';
 import {
+  setTableSort,
   setTimelineEndDate,
   setTimelineStartDate,
 } from '../../store/actions/configuration.actions';
@@ -22,12 +24,20 @@ export class ViewConfigurationService {
     return this.configuration.asObservable().pipe(map((config) => config.timeline));
   }
 
+  getTableConfiguration(): Observable<{ sort: MatSortable }> {
+    return this.configuration.asObservable().pipe(map((config) => config.table));
+  }
+
   changeTimelineStartDate(date: Date): void {
     this.store.dispatch(setTimelineStartDate({ startDate: date }));
   }
 
   changeTimelineEndDate(date: Date): void {
     this.store.dispatch(setTimelineEndDate({ endDate: date }));
+  }
+
+  changeTableSorting(sort: MatSortable): void {
+    this.store.dispatch(setTableSort({ sort }));
   }
 
   private initializeConfigurationBehaviorSubject(): void {
