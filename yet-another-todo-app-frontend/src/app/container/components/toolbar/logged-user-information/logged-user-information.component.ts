a30824408957040c66209307185a515f27183823
Krzysztof Pająk <chris.araneo@gmail.com>
2023-07-29 17:43:51 +0200

    Implement LoggedUserInformation component, update translations & format them

--- yet-another-todo-app-frontend/src/app/container/components/toolbar/logged-user-information/logged-user-information.component.ts
+++ yet-another-todo-app-frontend/src/app/container/components/toolbar/logged-user-information/logged-user-information.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'yata-logged-user-information',
   templateUrl: './logged-user-information.component.html',
   styleUrls: ['./logged-user-information.component.scss'],
 })
-export class LoggedUserInformationComponent {}
+export class LoggedUserInformationComponent {
+  @Input() username: string | null = null;
+}
