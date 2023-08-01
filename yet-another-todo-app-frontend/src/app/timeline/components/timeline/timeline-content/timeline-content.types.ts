7ad1af8e87353f6d428cabd1fde9cbe511490d3f
Krzysztof Pająk <chris.araneo@gmail.com>
2023-08-01 23:56:46 +0200

    Add missing type

--- yet-another-todo-app-frontend/src/app/timeline/components/timeline/timeline-content/timeline-content.types.ts
+++ yet-another-todo-app-frontend/src/app/timeline/components/timeline/timeline-content/timeline-content.types.ts
@@ -1,6 +1,11 @@
-import { StartedTask } from 'src/app/shared/models/task.model';
+import { StartedTask, Task } from 'src/app/shared/models/task.model';
 
 export type Column = {
   tasks: StartedTask[];
   left: string;
+};
+
+export type TimelineColumn = {
+  leftMargin: number;
+  tasks: Task[];
 };
