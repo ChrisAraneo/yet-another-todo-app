0ca1f6043c99d59899042b043eda7c91d1cc2e86
Krzysztof Pająk <chris.araneo@gmail.com>
2023-08-03 21:41:17 +0200

    Implement DragDropTaskOrderList, update translations

--- yet-another-todo-app-frontend/src/app/modals/components/configure-timeline-modal/drag-drop-task-order-list/drag-drop-task-order-list.component.spec.ts
+++ yet-another-todo-app-frontend/src/app/modals/components/configure-timeline-modal/drag-drop-task-order-list/drag-drop-task-order-list.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslatePipe } from '@ngx-translate/core';
+import { MockPipe } from 'ng-mocks';
 import { DragDropTaskOrderListComponent } from './drag-drop-task-order-list.component';
 
 describe('DragDropTaskOrderListComponent', () => {
@@ -7,7 +9,7 @@ describe('DragDropTaskOrderListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [DragDropTaskOrderListComponent],
+      declarations: [DragDropTaskOrderListComponent, MockPipe(TranslatePipe)],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DragDropTaskOrderListComponent);
--- yet-another-todo-app-frontend/src/app/modals/components/configure-timeline-modal/drag-drop-task-order-list/drag-drop-task-order-list.component.ts
+++ yet-another-todo-app-frontend/src/app/modals/components/configure-timeline-modal/drag-drop-task-order-list/drag-drop-task-order-list.component.ts
@@ -1,8 +1,55 @@
-import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { TaskState } from 'src/app/shared/models/task-state.model';
 
 @Component({
   selector: 'yata-drag-drop-task-order-list',
   templateUrl: './drag-drop-task-order-list.component.html',
   styleUrls: ['./drag-drop-task-order-list.component.scss'],
 })
-export class DragDropTaskOrderListComponent {}
+export class DragDropTaskOrderListComponent implements OnChanges {
+  @Input() states: TaskState[] = [];
+
+  @Output() changeStatesOrder: EventEmitter<string[]>;
+  @Output() changeStatesFilter: EventEmitter<string[]>;
+
+  values: string[];
+  checked: boolean[];
+
+  constructor() {
+    this.values = [];
+    this.checked = [];
+    this.changeStatesOrder = new EventEmitter<string[]>();
+    this.changeStatesFilter = new EventEmitter<string[]>();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const values = changes['states']?.currentValue.map((state: TaskState) => state.toString());
+
+    this.values = values;
+
+    while (this.checked.length > values.length) {
+      this.checked.pop();
+    }
+
+    while (this.checked.length < values.length) {
+      this.checked.push(true);
+    }
+  }
+
+  drop(event: CdkDragDrop<string[]>): void {
+    moveItemInArray(this.values, event.previousIndex, event.currentIndex);
+
+    this.emitValues();
+  }
+
+  emitValues(): void {
+    this.changeStatesOrder.next([...this.values]);
+
+    this.changeStatesFilter.next(
+      [...this.values].filter((_: string, index: number) => {
+        return this.checked[index] === true;
+      }),
+    );
+  }
+}
