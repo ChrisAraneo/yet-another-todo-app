3db45ef5261238a440d41dcbdb67319dc2822e58
Krzysztof Pająk <chris.araneo@gmail.com>
2023-08-03 21:42:44 +0200

    Update offline indicator unit test

--- yet-another-todo-app-frontend/src/app/container/components/toolbar/offline-indicator/offline-indicator.component.spec.ts
+++ yet-another-todo-app-frontend/src/app/container/components/toolbar/offline-indicator/offline-indicator.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslatePipe } from '@ngx-translate/core';
+import { MockPipe } from 'ng-mocks';
 import { OfflineIndicatorComponent } from './offline-indicator.component';
 
 describe('OfflineIndicatorComponent', () => {
@@ -7,7 +9,7 @@ describe('OfflineIndicatorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [OfflineIndicatorComponent],
+      declarations: [OfflineIndicatorComponent, MockPipe(TranslatePipe)],
     }).compileComponents();
 
     fixture = TestBed.createComponent(OfflineIndicatorComponent);
