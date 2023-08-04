068eae3f5584c9c3d2fcc0cedf6ee665e3bbae6d
Krzysztof Pająk <chris.araneo@gmail.com>
2023-08-04 22:39:33 +0200

    Add timeline configuration order & filter actions

--- yet-another-todo-app-frontend/src/app/shared/store/actions/configuration.actions.ts
+++ yet-another-todo-app-frontend/src/app/shared/store/actions/configuration.actions.ts
@@ -1,8 +1,11 @@
 import { MatSortable } from '@angular/material/sort';
 import { createAction, props } from '@ngrx/store';
+import { TaskState } from '../../models/task-state.model';
 
 const SET_TIMELINE_START_DATE = 'SET_TIMELINE_START_DATE';
 const SET_TIMELINE_END_DATE = 'SET_TIMELINE_END_DATE';
+const SET_TIMELINE_TASK_STATE_ORDER = 'SET_TIMELINE_TASK_STATE_ORDER';
+const SET_TIMELINE_TASK_STATE_FILTER = 'SET_TIMELINE_TASK_STATE_FILTER';
 const SET_TABLE_SORT = 'SET_TABLE_SORT';
 
 export const setTimelineStartDate = createAction(
@@ -10,4 +13,12 @@ export const setTimelineStartDate = createAction(
   props<{ startDate: Date }>(),
 );
 export const setTimelineEndDate = createAction(SET_TIMELINE_END_DATE, props<{ endDate: Date }>());
+export const setTimelineTaskStateOrder = createAction(
+  SET_TIMELINE_TASK_STATE_ORDER,
+  props<{ states: TaskState[] }>(),
+);
+export const setTimelineTaskStateFilter = createAction(
+  SET_TIMELINE_TASK_STATE_FILTER,
+  props<{ states: TaskState[] }>(),
+);
 export const setTableSort = createAction(SET_TABLE_SORT, props<{ sort: MatSortable }>());
