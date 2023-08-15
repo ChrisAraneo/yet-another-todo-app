d0beda3bee52e436f7f42c830124bd3cfdf4a105
Krzysztof Pająk <chris.araneo@gmail.com>
2023-08-15 17:23:53 +0200

    Update file input, display icons and filename

--- yet-another-todo-app-frontend/src/app/forms/components/file-input/file-input.component.ts
+++ yet-another-todo-app-frontend/src/app/forms/components/file-input/file-input.component.ts
@@ -15,29 +15,46 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 })
 export class FileInputComponent implements ControlValueAccessor {
   @Input() label: string = '';
+  @Input() selectFileLabel: string = '';
+  @Input() changeFileLabel: string = '';
 
   value: ArrayBuffer | null;
+  filename: string;
   isDisabled: boolean;
+  isLoading: boolean;
 
   changed?: (value: string) => void;
   touched?: () => void;
 
   constructor() {
     this.value = null;
+    this.filename = '';
     this.isDisabled = false;
+    this.isLoading = false;
   }
 
   onChange(event: Event): void {
+    if (this.isLoading || this.isDisabled) {
+      return;
+    }
+
     const inputElement: HTMLInputElement = <HTMLInputElement>event.target;
 
     if (typeof FileReader !== 'undefined') {
+      this.filename = '';
+      this.isLoading = true;
+
       const reader = new FileReader();
+      const file = (inputElement?.files as FileList)[0];
 
       reader.onload = (event: any): void => {
+        this.isLoading = false;
+        this.filename = file.name;
+
         this.changed && this.changed(event.target.result);
       };
 
-      reader.readAsArrayBuffer((inputElement?.files as FileList)[0]);
+      reader.readAsArrayBuffer(file);
     }
   }
 
--- yet-another-todo-app-frontend/src/app/forms/forms.module.ts
+++ yet-another-todo-app-frontend/src/app/forms/forms.module.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { MaterialModule } from '../shared/material.module';
 import { CancelButtonComponent } from './components/cancel-button/cancel-button.component';
 import { DatePickerComponent } from './components/date-picker/date-picker.component';
@@ -11,6 +14,10 @@ import { SubmitButtonComponent } from './components/submit-button/submit-button.
 import { TextInputComponent } from './components/text-input/text-input.component';
 import { TextareaComponent } from './components/textarea/textarea.component';
 import { DirectivesModule } from './directives/directives.module';
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -24,7 +31,19 @@ import { DirectivesModule } from './directives/directives.module';
     PasswordInputComponent,
     FileInputComponent,
   ],
-  imports: [BrowserModule, DirectivesModule, MaterialModule],
+  imports: [
+    BrowserModule,
+    DirectivesModule,
+    MaterialModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
   providers: [],
   exports: [
     SubmitButtonComponent,
