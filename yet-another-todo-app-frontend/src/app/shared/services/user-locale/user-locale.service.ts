68743e597f0c4ca99cc69fcd982a76eec4bf353e
Krzysztof Pająk <chris.araneo@gmail.com>
2023-08-20 18:05:18 +0200

    Implement #get

--- yet-another-todo-app-frontend/src/app/shared/services/user-locale/user-locale.service.ts
+++ yet-another-todo-app-frontend/src/app/shared/services/user-locale/user-locale.service.ts
@@ -1,6 +1,35 @@
 import { Injectable } from '@angular/core';
+import { enGB, pl } from 'date-fns/locale';
 
 @Injectable({
   providedIn: 'root',
 })
-export class UserLocaleService {}
+export class UserLocaleService {
+  get(): Locale {
+    const defaultValue = enGB;
+
+    if (typeof window === 'undefined' || typeof window.navigator === 'undefined') {
+      return defaultValue;
+    }
+
+    const navigator = window.navigator as any;
+    const localization =
+      (navigator.languages ? navigator.languages[0] : defaultValue) ||
+      navigator.language ||
+      navigator.browserLanguage ||
+      navigator.userLanguage;
+
+    const resolvedLanguage = localization.split('-')[0];
+
+    switch (resolvedLanguage) {
+      case 'en':
+        return enGB;
+      case 'pl':
+        return pl;
+      default:
+        console.error(`Application doesn't support users locale: ${resolvedLanguage}`);
+
+        return defaultValue;
+    }
+  }
+}
