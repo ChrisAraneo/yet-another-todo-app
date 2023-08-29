a29184f2d996aca0f83a39f7a5fe03f54f712ebf
Krzysztof Pająk <chris.araneo@gmail.com>
2023-08-30 00:29:22 +0200

    Implement NavigatorRefService

--- yet-another-todo-app-frontend/src/app/shared/services/navigator-ref/navigator-ref.service.ts
+++ yet-another-todo-app-frontend/src/app/shared/services/navigator-ref/navigator-ref.service.ts
@@ -1,6 +1,17 @@
-import { Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
-export class NavigatorRefService {}
+export class NavigatorRefService {
+  private window: Window | null;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    this.window = this.document?.defaultView || null;
+  }
+
+  get(): Navigator | null {
+    return this.window?.navigator || null;
+  }
+}
