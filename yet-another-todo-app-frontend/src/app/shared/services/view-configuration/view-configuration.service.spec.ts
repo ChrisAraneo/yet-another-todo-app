b0803852cbcb3e52beb66ec0cced38de6e3d98f2
Krzysztof Pająk <chris.araneo@gmail.com>
2023-08-30 22:42:19 +0200

    Add #getTimelineConfiguration and #getTableConfiguration unit tests

--- yet-another-todo-app-frontend/src/app/shared/services/view-configuration/view-configuration.service.spec.ts
+++ yet-another-todo-app-frontend/src/app/shared/services/view-configuration/view-configuration.service.spec.ts
@@ -1,15 +1,35 @@
 import { TestBed } from '@angular/core/testing';
 import { Store } from '@ngrx/store';
 import { MockProvider } from 'ng-mocks';
-import { of } from 'rxjs';
+import { first, of } from 'rxjs';
+import {
+  TableConfiguration,
+  TimelineConfiguration,
+} from '../../store/types/view-configuration.type';
 import { ViewConfigurationService } from './view-configuration.service';
 
-// TODO Unit tests for #setTimelineStartDate, #setTimelineEndDate and #getTimelineConfiguration
+// TODO Add remaining unit tests
 describe('ViewConfigurationService', () => {
   let service: ViewConfigurationService;
   let store: Store;
+  let timelineConfiguration: TimelineConfiguration;
+  let tableConfiguration: TableConfiguration;
 
   beforeEach(() => {
+    timelineConfiguration = {
+      startDate: new Date(),
+      endDate: new Date(),
+      order: [],
+      filter: [],
+    };
+    tableConfiguration = {
+      sort: {
+        id: 'creationDate',
+        start: 'desc',
+        disableClear: false,
+      },
+    };
+
     TestBed.configureTestingModule({
       imports: [],
       providers: [
@@ -17,10 +37,8 @@ describe('ViewConfigurationService', () => {
           select: (key: any) => {
             if (key === 'viewConfiguration') {
               return of({
-                timeline: {
-                  startDate: new Date(),
-                  endDate: new Date(),
-                },
+                timeline: { ...timelineConfiguration },
+                table: { ...tableConfiguration },
               });
             }
             return of(undefined);
@@ -28,11 +46,30 @@ describe('ViewConfigurationService', () => {
         }),
       ],
     });
+
     service = TestBed.inject(ViewConfigurationService);
     store = service.store;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('#getTimelineConfiguration should return timeline configuration object', () => {
+    service
+      .getTimelineConfiguration()
+      .pipe(first())
+      .subscribe((config) => {
+        expect(config).toEqual(timelineConfiguration);
+      });
+  });
+
+  it('#getTableConfiguration should return table configuration object', () => {
+    service
+      .getTableConfiguration()
+      .pipe(first())
+      .subscribe((config) => {
+        expect(config).toEqual(tableConfiguration);
+      });
   });
 });
