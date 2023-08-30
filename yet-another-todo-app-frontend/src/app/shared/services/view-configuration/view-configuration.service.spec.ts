b5e87ccaf94a63a9570417a8f2778f9e973227b0
Krzysztof Pająk <chris.araneo@gmail.com>
2023-08-30 22:56:07 +0200

    Add remaining ViewConfigurationService unit tests

--- yet-another-todo-app-frontend/src/app/shared/services/view-configuration/view-configuration.service.spec.ts
+++ yet-another-todo-app-frontend/src/app/shared/services/view-configuration/view-configuration.service.spec.ts
@@ -1,14 +1,22 @@
 import { TestBed } from '@angular/core/testing';
+import { MatSortable } from '@angular/material/sort';
 import { Store } from '@ngrx/store';
 import { MockProvider } from 'ng-mocks';
 import { first, of } from 'rxjs';
+import { InProgressTaskState, NotStartedTaskState } from '../../models/task-state.model';
+import {
+  setTableSort,
+  setTimelineEndDate,
+  setTimelineStartDate,
+  setTimelineTaskStateFilter,
+  setTimelineTaskStateOrder,
+} from '../../store/actions/configuration.actions';
 import {
   TableConfiguration,
   TimelineConfiguration,
 } from '../../store/types/view-configuration.type';
 import { ViewConfigurationService } from './view-configuration.service';
 
-// TODO Add remaining unit tests
 describe('ViewConfigurationService', () => {
   let service: ViewConfigurationService;
   let store: Store;
@@ -71,5 +79,59 @@ describe('ViewConfigurationService', () => {
       .subscribe((config) => {
         expect(config).toEqual(tableConfiguration);
       });
+  });
+
+  it('#changeTimelineStartDate should dispatch set timeline start date action', () => {
+    const dispatchSpy = spyOn(store, 'dispatch').and.callThrough();
+    const date = new Date('2023-10-01');
+    const action = setTimelineStartDate({ startDate: date });
+
+    service.changeTimelineStartDate(date);
+
+    expect(dispatchSpy).toHaveBeenCalledWith(action);
+  });
+
+  it('#changeTimelineEndDate should dispatch set timeline end date action', () => {
+    const dispatchSpy = spyOn(store, 'dispatch').and.callThrough();
+    const date = new Date('2023-10-02');
+    const action = setTimelineEndDate({ endDate: date });
+
+    service.changeTimelineEndDate(date);
+
+    expect(dispatchSpy).toHaveBeenCalledWith(action);
+  });
+
+  it('#changeTimelineColumnSorting should dispatch change timeline column sorting action', () => {
+    const dispatchSpy = spyOn(store, 'dispatch').and.callThrough();
+    const taskStatesInOrder = [new NotStartedTaskState(), new InProgressTaskState()];
+    const action = setTimelineTaskStateOrder({ states: taskStatesInOrder });
+
+    service.changeTimelineColumnSorting(taskStatesInOrder);
+
+    expect(dispatchSpy).toHaveBeenCalledWith(action);
+  });
+
+  it('#changeTimelineFiltering should dispatch set timeline filtering action', () => {
+    const dispatchSpy = spyOn(store, 'dispatch').and.callThrough();
+    const taskStatesToDisplay = [new NotStartedTaskState(), new InProgressTaskState()];
+    const action = setTimelineTaskStateFilter({ states: taskStatesToDisplay });
+
+    service.changeTimelineFiltering(taskStatesToDisplay);
+
+    expect(dispatchSpy).toHaveBeenCalledWith(action);
+  });
+
+  it('#changeTableSorting should dispatch set table sort action', () => {
+    const dispatchSpy = spyOn(store, 'dispatch').and.callThrough();
+    const sort: MatSortable = {
+      id: 'id',
+      start: 'asc',
+      disableClear: true,
+    };
+    const action = setTableSort({ sort });
+
+    service.changeTableSorting(sort);
+
+    expect(dispatchSpy).toHaveBeenCalledWith(action);
   });
 });
