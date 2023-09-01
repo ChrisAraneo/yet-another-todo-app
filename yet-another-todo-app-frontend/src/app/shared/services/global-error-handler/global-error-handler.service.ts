815bb77b02eb20003e0b7ed7db3c5c0f1c8972c2
Krzysztof Pająk <chris.araneo@gmail.com>
2023-09-01 23:53:35 +0200

    Implement GlobalErrorHandlerService

--- yet-another-todo-app-frontend/src/app/shared/services/global-error-handler/global-error-handler.service.ts
+++ yet-another-todo-app-frontend/src/app/shared/services/global-error-handler/global-error-handler.service.ts
@@ -1,6 +1,27 @@
-import { Injectable } from '@angular/core';
+import { ErrorHandler, Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ErrorSnackbarComponent } from '../../components/error-snackbar/error-snackbar.component';
 
 @Injectable({
   providedIn: 'root',
 })
-export class GlobalErrorHandlerService {}
+export class GlobalErrorHandlerService implements ErrorHandler {
+  private readonly durationInSeconds = 5;
+
+  constructor(private snackBar: MatSnackBar) {}
+
+  handleError(error: any): void {
+    console.error(error?.message);
+
+    this.openErrorSnackBar(error);
+  }
+
+  private openErrorSnackBar(error: any): void {
+    this.snackBar.openFromComponent(ErrorSnackbarComponent, {
+      duration: this.durationInSeconds * 1000,
+      data: error?.message,
+      horizontalPosition: 'end',
+      verticalPosition: 'bottom',
+    });
+  }
+}
