d099c3efc272df541ce0c10be72630dfbd84696e
Krzysztof Pająk <chris.araneo@gmail.com>
2023-09-09 23:30:59 +0200

    Add #getUser unit test

--- yet-another-todo-app-backend/src/prisma/prisma.service.spec.ts
+++ yet-another-todo-app-backend/src/prisma/prisma.service.spec.ts
@@ -1,6 +1,7 @@
 import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { DummyData } from '../../test/dummy-data';
 import { PrismaService } from './prisma.service';
 
 describe('PrismaService', () => {
@@ -17,5 +18,20 @@ describe('PrismaService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getUser', () => {
+    it('should find unique user when provided username', async () => {
+      const spy = jest.spyOn(service.user, 'findUnique');
+      const username = DummyData.user.username;
+
+      service.getUser(username);
+
+      expect(spy).toHaveBeenCalledWith({
+        where: {
+          username,
+        },
+      });
+    });
   });
 });
