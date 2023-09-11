2d33e41e7f72f4ffc45a51f68aa9c547460cccd6
Krzysztof Pająk <chris.araneo@gmail.com>
2023-09-11 22:13:05 +0200

    Update unit tests

--- yet-another-todo-app-frontend/src/app/shared/components/error-snackbar/error-snackbar.component.spec.ts
+++ yet-another-todo-app-frontend/src/app/shared/components/error-snackbar/error-snackbar.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ErrorSnackbarComponent } from './error-snackbar.component';
+import { MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
 
 describe('ErrorSnackbarComponent', () => {
   let component: ErrorSnackbarComponent;
@@ -8,6 +9,7 @@ describe('ErrorSnackbarComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ErrorSnackbarComponent],
+      providers: [{ provide: MAT_SNACK_BAR_DATA, useValue: {} }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ErrorSnackbarComponent);
--- yet-another-todo-app-frontend/src/app/shared/services/global-error-handler/global-error-handler.service.spec.ts
+++ yet-another-todo-app-frontend/src/app/shared/services/global-error-handler/global-error-handler.service.spec.ts
@@ -1,11 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 import { GlobalErrorHandlerService } from './global-error-handler.service';
+import { MockProvider } from 'ng-mocks';
+import { MatSnackBar, MatSnackBarRef } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
 
 describe('GlobalErrorHandlerService', () => {
   let service: GlobalErrorHandlerService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [MatDialogModule],
+      providers: [
+        MockProvider(MatSnackBar, {
+          openFromComponent: (): MatSnackBarRef<any> => {
+            return null as unknown as MatSnackBarRef<any>;
+          },
+        }),
+      ],
+    });
+
     service = TestBed.inject(GlobalErrorHandlerService);
   });
 
