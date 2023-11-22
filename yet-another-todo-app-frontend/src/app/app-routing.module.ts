56086e40e6f6c3f14b472f0b9be0f49c0ebba46e
Krzysztof Pająk <chris.araneo@gmail.com>
2023-11-22 22:49:32 +0100

    Add timeline and table routes

--- yet-another-todo-app-frontend/src/app/app-routing.module.ts
+++ yet-another-todo-app-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TableComponent } from './table/components/table/table.component';
+import { TimelineComponent } from './timeline/components/timeline/timeline.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'timeline', component: TimelineComponent },
+  { path: 'table', component: TableComponent },
+  { path: '', pathMatch: 'prefix', redirectTo: 'timeline' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
