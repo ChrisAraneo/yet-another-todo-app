577a6bfaf1b100e0f895162a239e07217c757ef4
Krzysztof Pająk <chris.araneo@gmail.com>
2023-12-09 14:32:51 +0100

    Fix redirect after closing AddTaskModal

--- yet-another-todo-app-frontend/src/app/modals/components/add-task-modal-launcher/add-task-modal-launcher.component.ts
+++ yet-another-todo-app-frontend/src/app/modals/components/add-task-modal-launcher/add-task-modal-launcher.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Subscription, debounceTime, first } from 'rxjs';
+import { Subscription, debounceTime, first, mergeMap } from 'rxjs';
+import { TABLE_PATH, TIMELINE_PATH } from 'src/app/app-routing.consts';
+import { ViewConfigurationService } from 'src/app/shared/services/view-configuration/view-configuration.service';
+import { AppMode } from 'src/app/shared/store/types/view-configuration.type';
 import { DialogService } from '../../services/dialog/dialog.service';
 
 @Component({
@@ -11,7 +14,11 @@ import { DialogService } from '../../services/dialog/dialog.service';
 export class AddTaskModalLauncherComponent implements OnInit, OnDestroy {
   private subscription?: Subscription;
 
-  constructor(private dialogService: DialogService, private router: Router) {}
+  constructor(
+    private dialogService: DialogService,
+    private router: Router,
+    private viewConfigurationService: ViewConfigurationService,
+  ) {}
 
   ngOnInit(): void {
     this.subscribeToModalClose();
@@ -24,9 +31,17 @@ export class AddTaskModalLauncherComponent implements OnInit, OnDestroy {
   private subscribeToModalClose(): void {
     const subscription = this.dialogService
       .openAddTaskModal()
-      .pipe(debounceTime(150), first())
-      .subscribe(() => {
-        this.router.navigate(['..']);
+      .pipe(
+        debounceTime(150),
+        first(),
+        mergeMap(() => {
+          return this.viewConfigurationService.getAppMode();
+        }),
+      )
+      .subscribe((mode: AppMode) => {
+        if (mode === AppMode.Timeline || mode === AppMode.Table) {
+          this.router.navigate([mode === AppMode.Timeline ? TIMELINE_PATH : TABLE_PATH]);
+        }
       });
 
     if (!this.subscription) {
