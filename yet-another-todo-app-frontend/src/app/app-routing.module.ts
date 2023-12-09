de252e036ceee3d5cecf555b173877612ea731df
Krzysztof Pająk <chris.araneo@gmail.com>
2023-12-09 14:32:16 +0100

    Replace strings with path consts

--- yet-another-todo-app-frontend/src/app/app-routing.module.ts
+++ yet-another-todo-app-frontend/src/app/app-routing.module.ts
@@ -1,28 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ADD_TASK_PATH, TABLE_PATH, TIMELINE_PATH } from './app-routing.consts';
 import { AddTaskModalLauncherComponent } from './modals/components/add-task-modal-launcher/add-task-modal-launcher.component';
 import { TableComponent } from './table/components/table/table.component';
 import { TimelineComponent } from './timeline/components/timeline/timeline.component';
 
 const modalRoutes: Routes = [
   {
-    path: 'add-task',
+    path: ADD_TASK_PATH,
     component: AddTaskModalLauncherComponent,
   },
 ];
 
 const routes: Routes = [
   {
-    path: 'timeline',
+    path: TIMELINE_PATH,
     component: TimelineComponent,
     children: modalRoutes,
   },
   {
-    path: 'table',
+    path: TABLE_PATH,
     component: TableComponent,
     children: modalRoutes,
   },
-  { path: '', pathMatch: 'prefix', redirectTo: 'timeline' },
+  { path: '', pathMatch: 'full', redirectTo: TIMELINE_PATH },
 ];
 
 @NgModule({
