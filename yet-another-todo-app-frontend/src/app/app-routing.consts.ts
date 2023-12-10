fc9221a5c5262e9f4f2d527a90b4755913f4ccb9
Krzysztof Pająk <chris.araneo@gmail.com>
2023-12-10 16:33:08 +0100

    Add delete task path const

--- yet-another-todo-app-frontend/src/app/app-routing.consts.ts
+++ yet-another-todo-app-frontend/src/app/app-routing.consts.ts
@@ -2,3 +2,4 @@ export const TIMELINE_PATH = 'timeline';
 export const TABLE_PATH = 'table';
 export const ADD_TASK_PATH = 'add-task';
 export const EDIT_TASK_PATH = 'edit-task';
+export const DELETE_TASK_PATH = 'delete-task';
