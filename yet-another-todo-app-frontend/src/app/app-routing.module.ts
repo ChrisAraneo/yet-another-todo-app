ad8939a069319de962b9cf7f9101556464ea5e88
Krzysztof Pająk <chris.araneo@gmail.com>
2023-12-10 14:49:42 +0100

    Add edit task route

--- yet-another-todo-app-frontend/src/app/app-routing.module.ts
+++ yet-another-todo-app-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ADD_TASK_PATH, TABLE_PATH, TIMELINE_PATH } from './app-routing.consts';
+import { ADD_TASK_PATH, EDIT_TASK_PATH, TABLE_PATH, TIMELINE_PATH } from './app-routing.consts';
 import { AddTaskModalLauncherComponent } from './modals/components/add-task-modal-launcher/add-task-modal-launcher.component';
+import { EditTaskModalLauncherComponent } from './modals/components/edit-task-modal-launcher/edit-task-modal-launcher.component';
 import { TableComponent } from './table/components/table/table.component';
 import { TimelineComponent } from './timeline/components/timeline/timeline.component';
 
@@ -9,6 +10,10 @@ const modalRoutes: Routes = [
   {
     path: ADD_TASK_PATH,
     component: AddTaskModalLauncherComponent,
+  },
+  {
+    path: EDIT_TASK_PATH,
+    component: EditTaskModalLauncherComponent,
   },
 ];
 
