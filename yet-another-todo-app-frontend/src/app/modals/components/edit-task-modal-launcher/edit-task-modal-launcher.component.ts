6d204cf05f71cf0ed4b35538e2b5b4ffbde84654
Krzysztof Pająk <chris.araneo@gmail.com>
2023-12-10 14:49:09 +0100

    Implement EditTaskModalLauncher

--- yet-another-todo-app-frontend/src/app/modals/components/edit-task-modal-launcher/edit-task-modal-launcher.component.ts
+++ yet-another-todo-app-frontend/src/app/modals/components/edit-task-modal-launcher/edit-task-modal-launcher.component.ts
@@ -1,8 +1,51 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription, debounceTime, first, mergeMap } from 'rxjs';
+import { TABLE_PATH, TIMELINE_PATH } from 'src/app/app-routing.consts';
+import { ViewConfigurationService } from 'src/app/shared/services/view-configuration/view-configuration.service';
+import { AppMode } from 'src/app/shared/store/types/view-configuration.type';
+import { DialogService } from '../../services/dialog/dialog.service';
 
 @Component({
   selector: 'yata-edit-task-modal-launcher',
   templateUrl: './edit-task-modal-launcher.component.html',
   styleUrls: ['./edit-task-modal-launcher.component.scss'],
 })
-export class EditTaskModalLauncherComponent {}
+export class EditTaskModalLauncherComponent {
+  private subscription?: Subscription;
+
+  constructor(
+    private dialogService: DialogService,
+    private router: Router,
+    private viewConfigurationService: ViewConfigurationService,
+  ) {}
+
+  ngOnInit(): void {
+    this.subscribeToModalClose();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription && this.subscription.unsubscribe();
+  }
+
+  private subscribeToModalClose(): void {
+    const subscription = this.dialogService
+      .openEditTaskModal()
+      .pipe(
+        debounceTime(150),
+        first(),
+        mergeMap(() => this.viewConfigurationService.getAppMode()),
+      )
+      .subscribe((mode: AppMode) => {
+        if (mode === AppMode.Timeline || mode === AppMode.Table) {
+          this.router.navigate([mode === AppMode.Timeline ? TIMELINE_PATH : TABLE_PATH]);
+        }
+      });
+
+    if (!this.subscription) {
+      this.subscription = subscription;
+    } else {
+      this.subscription.add(subscription);
+    }
+  }
+}
