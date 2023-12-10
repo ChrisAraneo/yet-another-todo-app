62bf4d4cbe0ff258b8f5c5289fa08e642b923751
Krzysztof Pająk <chris.araneo@gmail.com>
2023-12-10 16:34:55 +0100

    Add delete task route

--- yet-another-todo-app-frontend/src/app/app-routing.module.ts
+++ yet-another-todo-app-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ADD_TASK_PATH, EDIT_TASK_PATH, TABLE_PATH, TIMELINE_PATH } from './app-routing.consts';
+import {
+  ADD_TASK_PATH,
+  DELETE_TASK_PATH,
+  EDIT_TASK_PATH,
+  TABLE_PATH,
+  TIMELINE_PATH,
+} from './app-routing.consts';
 import { AddTaskModalLauncherComponent } from './modals/components/add-task-modal-launcher/add-task-modal-launcher.component';
+import { DeleteTaskModalLauncherComponent } from './modals/components/delete-task-modal-launcher/delete-task-modal-launcher.component';
 import { EditTaskModalLauncherComponent } from './modals/components/edit-task-modal-launcher/edit-task-modal-launcher.component';
 import { TableComponent } from './table/components/table/table.component';
 import { TimelineComponent } from './timeline/components/timeline/timeline.component';
@@ -14,6 +21,10 @@ const modalRoutes: Routes = [
   {
     path: EDIT_TASK_PATH,
     component: EditTaskModalLauncherComponent,
+  },
+  {
+    path: DELETE_TASK_PATH,
+    component: DeleteTaskModalLauncherComponent,
   },
 ];
 
