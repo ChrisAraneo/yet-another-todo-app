6db27efaf886615ceac5ca8e109d475cf39ee3e4
Krzysztof Pająk <chris.araneo@gmail.com>
2024-01-16 21:53:43 +0100

    Add export tasks path const

--- yet-another-todo-app-frontend/src/app/app-routing.consts.ts
+++ yet-another-todo-app-frontend/src/app/app-routing.consts.ts
@@ -3,3 +3,4 @@ export const TABLE_PATH = 'table';
 export const ADD_TASK_PATH = 'add-task';
 export const EDIT_TASK_PATH = 'edit-task';
 export const DELETE_TASK_PATH = 'delete-task';
+export const EXPORT_TASKS_PATH = 'export-tasks';
