c64e00cfcebef46f2a3b7c9f7b7a13415db57018
Krzysztof Pająk <chris.araneo@gmail.com>
2024-01-21 19:51:08 +0100

    Update unit tests

--- yet-another-todo-app-frontend/src/app/modals/components/modal-launcher/modal-launcher.component.spec.ts
+++ yet-another-todo-app-frontend/src/app/modals/components/modal-launcher/modal-launcher.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MockProvider } from 'ng-mocks';
+import { ViewConfigurationService } from 'src/app/shared/services/view-configuration/view-configuration.service';
+import { DialogService } from '../../services/dialog/dialog.service';
 import { ModalLauncherComponent } from './modal-launcher.component';
 
 describe('ModalLauncherComponent', () => {
@@ -8,6 +12,12 @@ describe('ModalLauncherComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ModalLauncherComponent],
+      providers: [
+        MockProvider(DialogService),
+        MockProvider(ActivatedRoute),
+        MockProvider(Router),
+        MockProvider(ViewConfigurationService),
+      ],
     });
     fixture = TestBed.createComponent(ModalLauncherComponent);
     component = fixture.componentInstance;
