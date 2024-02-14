6438585b38a33c25f5867f729e15847b3aa7fd3a
Krzysztof Pająk <chris.araneo@gmail.com>
2024-02-14 22:09:07 +0100

    Implement basic stepper features, add styles, define structure in template

--- yet-another-todo-app-frontend/src/app/forms/components/stepper/stepper.component.ts
+++ yet-another-todo-app-frontend/src/app/forms/components/stepper/stepper.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Step } from './stepper.types';
 
 @Component({
   selector: 'yata-stepper',
   templateUrl: './stepper.component.html',
   styleUrls: ['./stepper.component.scss'],
 })
-export class StepperComponent {}
+export class StepperComponent implements OnInit {
+  @Input() steps: Step[] = [];
+  @Input('activeIndex') initialIndex: number = 0;
+
+  @Output() changeIndex = new EventEmitter<number>();
+
+  activeIndex!: number;
+
+  ngOnInit(): void {
+    this.activeIndex = this.initialIndex;
+  }
+
+  onClick(index: number): void {
+    this.activeIndex = index;
+    this.changeIndex.next(index);
+  }
+}
