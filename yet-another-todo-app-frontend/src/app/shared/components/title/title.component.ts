fa5345e1d005802f6c8c701f201f3b22286c636d
Krzysztof Pająk <chris.araneo@gmail.com>
2024-02-16 21:45:50 +0100

    Add text align input to title, refactor

--- yet-another-todo-app-frontend/src/app/shared/components/title/title.component.ts
+++ yet-another-todo-app-frontend/src/app/shared/components/title/title.component.ts
@@ -1,10 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'yata-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss'],
 })
-export class TitleComponent {
+export class TitleComponent implements OnInit {
   @Input() color: string = '';
+  @Input() textAlign: string = '';
+
+  style!: object;
+
+  ngOnInit(): void {
+    this.style = {
+      color: this.color || null,
+      'text-align': this.textAlign || null,
+    };
+  }
 }
