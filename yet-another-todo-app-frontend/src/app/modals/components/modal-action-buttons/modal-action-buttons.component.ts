896178f6f973f6226eb3cf8098c49bbc8c0f6203
Krzysztof Pająk <chris.araneo@gmail.com>
2024-02-17 19:58:58 +0100

    Add next and back button support in modal action buttons

--- yet-another-todo-app-frontend/src/app/modals/components/modal-action-buttons/modal-action-buttons.component.ts
+++ yet-another-todo-app-frontend/src/app/modals/components/modal-action-buttons/modal-action-buttons.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { CancelButton, SubmitButton } from './modal-action-buttons.types';
+import { BackButton, CancelButton, NextButton, SubmitButton } from './modal-action-buttons.types';
 
 const NOOP = async (): Promise<void> => {
   return;
@@ -13,11 +13,24 @@ const NOOP = async (): Promise<void> => {
 })
 export class ModalActionButtonsComponent {
   @Input() form?: FormGroup<any>;
+  @Input() step: number = 1;
+  @Input() total: number = 1;
+  @Input() nextButton: NextButton = {
+    label: '',
+    color: '',
+    click: NOOP,
+  };
+  @Input() backButton: BackButton = {
+    label: '',
+    color: '',
+    click: NOOP,
+  };
   @Input() submitButton: SubmitButton = {
     label: '',
     color: 'primary',
     click: NOOP,
   };
+  // TODO Remove cancel button
   @Input() cancelButton: CancelButton = {
     label: '',
     click: NOOP,
--- yet-another-todo-app-frontend/src/app/modals/components/modal-action-buttons/modal-action-buttons.types.ts
+++ yet-another-todo-app-frontend/src/app/modals/components/modal-action-buttons/modal-action-buttons.types.ts
@@ -1,3 +1,18 @@
+export type NextButton = {
+  click: (event?: any) => Promise<void>;
+
+  color?: string;
+  icon?: string;
+  label?: string;
+};
+
+export type BackButton = {
+  click: (event?: any) => Promise<void>;
+  color?: string;
+  icon?: string;
+  label?: string;
+};
+
 export type SubmitButton = {
   label: string;
   click: (event?: any) => Promise<void>;
@@ -5,6 +20,7 @@ export type SubmitButton = {
   icon?: string;
 };
 
+/* TODO REMOVE */
 export type CancelButton = {
   label: string;
   click: (() => Promise<void>) | (() => void);
