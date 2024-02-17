9eeb0ee184390625f01b306bc4284ac86ff18705
Krzysztof Pająk <chris.araneo@gmail.com>
2024-02-17 19:17:39 +0100

    Define basic component structure, set styles, implement on click logic

--- yet-another-todo-app-frontend/src/app/forms/components/next-button/next-button.component.ts
+++ yet-another-todo-app-frontend/src/app/forms/components/next-button/next-button.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+const NOOP = async (): Promise<void> => {
+  return;
+};
 
 @Component({
   selector: 'yata-next-button',
   templateUrl: './next-button.component.html',
   styleUrls: ['./next-button.component.scss'],
 })
-export class NextButtonComponent {}
+export class NextButtonComponent {
+  @Input() next: (event?: any) => Promise<void> = NOOP;
+  @Input() color?: string = '';
+  @Input() icon?: string = 'navigate_next';
+
+  isLoading: boolean = false;
+
+  onButtonClick(event?: any): void {
+    this.isLoading = true;
+
+    this.next(event).then(() => (this.isLoading = false));
+  }
+}
