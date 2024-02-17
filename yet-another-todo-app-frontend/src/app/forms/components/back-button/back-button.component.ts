dc2ba108066f643d80567565b84f2a2c45c80674
Krzysztof Pająk <chris.araneo@gmail.com>
2024-02-17 19:53:36 +0100

    Define basic component structure, set styles, implement on click logic

--- yet-another-todo-app-frontend/src/app/forms/components/back-button/back-button.component.ts
+++ yet-another-todo-app-frontend/src/app/forms/components/back-button/back-button.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+const NOOP = async (): Promise<void> => {
+  return;
+};
 
 @Component({
   selector: 'yata-back-button',
   templateUrl: './back-button.component.html',
   styleUrls: ['./back-button.component.scss'],
 })
-export class BackButtonComponent {}
+export class BackButtonComponent {
+  @Input() back: (event?: any) => Promise<void> = NOOP;
+  @Input() color?: string = '';
+  @Input() icon?: string = 'navigate_before';
+
+  isLoading: boolean = false;
+
+  onButtonClick(event?: any): void {
+    this.isLoading = true;
+
+    this.back(event).then(() => (this.isLoading = false));
+  }
+}
