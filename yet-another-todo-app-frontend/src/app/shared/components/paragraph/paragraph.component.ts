aa811d1353226ed9394052f859ebd93423a7e46f
Krzysztof Pająk <chris.araneo@gmail.com>
2024-03-06 05:20:42 +0100

    Create ParagraphComponent

---
+++ yet-another-todo-app-frontend/src/app/shared/components/paragraph/paragraph.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'yata-paragraph',
+  templateUrl: './paragraph.component.html',
+  styleUrls: ['./paragraph.component.scss'],
+})
+export class ParagraphComponent {}
