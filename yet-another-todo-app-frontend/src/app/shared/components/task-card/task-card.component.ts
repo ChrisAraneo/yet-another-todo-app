4d5543572ff0ccc4851a6d6247571d1a9f95da81
Krzysztof Pająk <chris.araneo@gmail.com>
2024-03-10 09:52:48 +0100

    Create new TaskCard shared component

---
+++ yet-another-todo-app-frontend/src/app/shared/components/task-card/task-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'yata-task-card',
+  templateUrl: './task-card.component.html',
+  styleUrls: ['./task-card.component.scss'],
+})
+export class TaskCardComponent {}
