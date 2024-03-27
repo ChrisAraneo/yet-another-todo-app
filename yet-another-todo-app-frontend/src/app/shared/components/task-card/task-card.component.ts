0c334afba111bc60c4dd281a8a947df9572d972c
Krzysztof Pająk <chris.araneo@gmail.com>
2024-03-27 21:04:51 +0100

    Display start date and end date on TaskCard

--- yet-another-todo-app-frontend/src/app/shared/components/task-card/task-card.component.ts
+++ yet-another-todo-app-frontend/src/app/shared/components/task-card/task-card.component.ts
@@ -1,20 +1,41 @@
-import { Component, Input, OnDestroy } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { DialogService } from 'src/app/modals/services/dialog/dialog.service';
-import { Task } from '../../models/task.model';
+import { EndedTask, StartedTask, Task } from '../../models/task.model';
+import { DateUtilsService } from '../../services/date-utils/date-utils.service';
 
 @Component({
   selector: 'yata-task-card',
   templateUrl: './task-card.component.html',
   styleUrls: ['./task-card.component.scss'],
 })
-export class TaskCardComponent implements OnDestroy {
+export class TaskCardComponent implements OnChanges, OnDestroy {
   @Input() task!: Task;
+
+  startDate: string = '';
+  startTime: string = '';
+  endDate: string = '';
+  endTime: string = '';
 
   private subscription: Subscription;
 
-  constructor(private dialogService: DialogService) {
+  constructor(private dialogService: DialogService, private dateUtilsService: DateUtilsService) {
     this.subscription = new Subscription();
+  }
+
+  ngOnChanges(): void {
+    const startDate = this.task instanceof StartedTask ? this.task.getStartDate() : null;
+    const endDate = this.task instanceof EndedTask ? this.task.getEndDate() : null;
+
+    if (startDate) {
+      this.startDate = `${this.dateUtilsService.formatDate(startDate, 'dd MMM')}`;
+      this.startTime = this.dateUtilsService.formatDate(startDate, 'HH:mm');
+    }
+
+    if (endDate) {
+      this.endDate = this.dateUtilsService.formatDate(endDate, 'dd MMM');
+      this.endTime = this.dateUtilsService.formatDate(endDate, 'HH:mm');
+    }
   }
 
   ngOnDestroy(): void {
--- yet-another-todo-app-frontend/src/app/shared/components/task-card/task-card.module.ts
+++ yet-another-todo-app-frontend/src/app/shared/components/task-card/task-card.module.ts
@@ -1,7 +1,13 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpLoaderFactory } from 'src/app/app.module';
+import { DateUtilsService } from '../../services/date-utils/date-utils.service';
+import { LabelModule } from '../label/label.module';
 import { ParagraphModule } from '../paragraph/paragraph.module';
 import { TaskStateIconModule } from '../task-state-icon/task-state-icon.module';
 import { TitleModule } from '../title/title.module';
@@ -10,14 +16,24 @@ import { TaskCardComponent } from './task-card.component';
 @NgModule({
   declarations: [TaskCardComponent],
   imports: [
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
     CommonModule,
     MatButtonModule,
     TaskStateIconModule,
     MatIconModule,
     TitleModule,
+    LabelModule,
     ParagraphModule,
+    MatTooltipModule,
   ],
-  providers: [],
+  providers: [DateUtilsService],
   exports: [TaskCardComponent],
 })
 export class TaskCardModule {}
