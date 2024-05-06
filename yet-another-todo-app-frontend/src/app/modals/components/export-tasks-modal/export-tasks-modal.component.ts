1c2b7a54c28420a6d8720a56337cd88e32a8496e
Krzysztof Pająk <chris.araneo@gmail.com>
2024-05-06 22:21:52 +0200

    Update export tasks modal and implement new modal design and structure

--- yet-another-todo-app-frontend/src/app/modals/components/export-tasks-modal/export-tasks-modal.component.ts
+++ yet-another-todo-app-frontend/src/app/modals/components/export-tasks-modal/export-tasks-modal.component.ts
@@ -1,3 +1,4 @@
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
@@ -12,6 +13,18 @@ import { ExportTasksForm } from './export-tasks-modal.types';
   selector: 'yata-export-tasks-modal',
   templateUrl: './export-tasks-modal.component.html',
   styleUrls: ['./export-tasks-modal.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(
+          '300ms',
+          keyframes([style({ opacity: 0 }), style({ opacity: 0 }), style({ opacity: 1 })]),
+        ),
+      ]),
+      transition(':leave', [animate('150ms', style({ opacity: 0 }))]),
+    ]),
+  ],
 })
 export class ExportTasksModalComponent {
   static readonly PANEL_CLASS = 'export-tasks-modal';
