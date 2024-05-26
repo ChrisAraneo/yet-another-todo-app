55dbdcfc1357e75698c2df9eef7a06e293ebdfc9
Krzysztof Pająk <chris.araneo@gmail.com>
2024-05-26 15:18:10 +0200

    Revert old select because of issues with prime-ng dropdown

--- yet-another-todo-app-frontend/src/app/forms/components/select/select.component.ts
+++ yet-another-todo-app-frontend/src/app/forms/components/select/select.component.ts
@@ -1,4 +1,4 @@
-import { Component, forwardRef, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, forwardRef, Input, ViewChild } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import diff from 'microdiff';
 import { Option } from './select.types';
@@ -15,20 +15,36 @@ import { Option } from './select.types';
     },
   ],
 })
-export class SelectComponent implements ControlValueAccessor {
+export class SelectComponent implements ControlValueAccessor, AfterViewInit {
+  @ViewChild('select') selectElementRef!: ElementRef;
+
   @Input() label: string = '';
   @Input() options: Option<any>[] = [];
 
-  selectedOption?: Option<any>;
-  value?: any;
+  value: any;
+  isDisabled: boolean;
 
   changed?: (value: any) => void;
   touched?: () => void;
 
-  onChange(event: Event): void {
-    const value = this.options.find((item) => this.isEqual(item, event))?.value;
+  constructor() {
+    this.isDisabled = false;
+  }
 
-    this.changed && this.changed(value);
+  ngAfterViewInit(): void {
+    const index: number = this.options
+      .map((option) => option.value)
+      .findIndex((value) => !diff(value, this.value).length);
+
+    this.selectElementRef.nativeElement.value = index >= 0 ? index : 0;
+  }
+
+  onChange(event: Event): void {
+    const selectedIndex: string = (<HTMLInputElement>event.target).value;
+    const selectedOption = this.options[+selectedIndex];
+
+    this.value = selectedOption.value;
+    this.changed && this.changed(selectedOption.value);
   }
 
   onBlur(): void {
@@ -36,12 +52,7 @@ export class SelectComponent implements ControlValueAccessor {
   }
 
   writeValue(value: any): void {
-    this.options.forEach((item, index) => {
-      if (this.isEqual(item?.value, value)) {
-        this.value = item?.value;
-        this.selectedOption = this.options[index];
-      }
-    });
+    this.value = value;
   }
 
   registerOnChange(fn: any): void {
@@ -52,11 +63,7 @@ export class SelectComponent implements ControlValueAccessor {
     this.touched = fn;
   }
 
-  private isEqual(a: any, b: any): boolean {
-    if (typeof a === 'object' && typeof b === 'object') {
-      return !diff(a, b).length;
-    } else {
-      return a === b;
-    }
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
   }
 }
