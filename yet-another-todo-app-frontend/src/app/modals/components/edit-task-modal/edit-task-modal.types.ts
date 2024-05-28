2a50fadb9713fc8b1d47b16a32c7a346471b5987
Krzysztof Pająk <chris.araneo@gmail.com>
2024-05-28 22:14:33 +0200

    Create EditTaskModalData type

--- yet-another-todo-app-frontend/src/app/modals/components/edit-task-modal/edit-task-modal.types.ts
+++ yet-another-todo-app-frontend/src/app/modals/components/edit-task-modal/edit-task-modal.types.ts
@@ -2,6 +2,10 @@ import { FormControl } from '@angular/forms';
 import { TaskState } from '../../../shared/models/task-state.model';
 import { Task } from '../../../shared/models/task.model';
 
+export type EditTaskModalData = {
+  initialTaskId: string;
+};
+
 export type TaskForm = {
   task: FormControl<Task | null>;
   title: FormControl<string>;
