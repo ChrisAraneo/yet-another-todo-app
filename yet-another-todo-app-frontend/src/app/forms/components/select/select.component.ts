922b71e131a8266de60c8bf34109e1cb712fced7
Krzysztof Pająk <chris.araneo@gmail.com>
2024-06-08 17:20:15 +0200

    Implement custom select dropdown panel and items

--- yet-another-todo-app-frontend/src/app/forms/components/select/select.component.ts
+++ yet-another-todo-app-frontend/src/app/forms/components/select/select.component.ts
@@ -1,7 +1,10 @@
 import { AfterViewInit, Component, ElementRef, forwardRef, Input, ViewChild } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
-import diff from 'microdiff';
 import { Option } from './select.types';
+
+// TODO Fix hover cursor
+// TODO Fix dropdown icon animation
+// TODO Implement searching items
 
 @Component({
   selector: 'yata-select',
@@ -16,35 +19,65 @@ import { Option } from './select.types';
   ],
 })
 export class SelectComponent implements ControlValueAccessor, AfterViewInit {
-  @ViewChild('select') selectElementRef!: ElementRef;
+  @ViewChild('input') inputElementRef!: ElementRef;
 
   @Input() label: string = '';
   @Input() options: Option<any>[] = [];
 
+  text: string;
   value: any;
+  dropdown = {
+    width: '0',
+    isOpened: false,
+  };
   isDisabled: boolean;
+  selectedIndex: number;
+
+  dropdownStyles: { [key: string]: string | number };
 
   changed?: (value: any) => void;
   touched?: () => void;
 
   constructor() {
+    this.text = '';
+    this.value = null;
+    this.dropdown.width = '0';
+    this.dropdown.isOpened = false;
     this.isDisabled = false;
+    this.dropdownStyles = {};
+    this.selectedIndex = -1;
   }
 
   ngAfterViewInit(): void {
-    const index: number = this.options
-      .map((option) => option.value)
-      .findIndex((value) => !diff(value, this.value).length);
-
-    this.selectElementRef.nativeElement.value = index >= 0 ? index : 0;
+    setTimeout(() => {
+      this.text = this.options.find((item) => item.value === this.value)?.label || '';
+      this.dropdown.width = `${this.inputElementRef.nativeElement?.offsetWidth || 0}px`;
+    });
   }
 
-  onChange(event: Event): void {
-    const selectedIndex: string = (<HTMLInputElement>event.target).value;
+  selectOption(event: Event, selectedIndex: number): void {
+    event.preventDefault();
+
     const selectedOption = this.options[+selectedIndex];
 
     this.value = selectedOption.value;
+    this.text = selectedOption.label;
+    this.dropdown.isOpened = false;
+    this.selectedIndex = selectedIndex;
     this.changed && this.changed(selectedOption.value);
+  }
+
+  onChange(event: Event): void {
+    const selectedIndex = +(<HTMLInputElement>event.target).value;
+    const selectedOption = this.options[selectedIndex];
+
+    this.value = selectedOption.value;
+    this.selectedIndex = selectedIndex;
+    this.changed && this.changed(selectedOption.value);
+  }
+
+  onFocus(): void {
+    this.dropdown.isOpened = true;
   }
 
   onBlur(): void {
@@ -53,6 +86,7 @@ export class SelectComponent implements ControlValueAccessor, AfterViewInit {
 
   writeValue(value: any): void {
     this.value = value;
+    this.selectedIndex = this.options.findIndex((item) => item.value === value);
   }
 
   registerOnChange(fn: any): void {
