c32473f389a1878cc3ce88185645ead56cfb5b51
Krzysztof Pająk <chris.araneo@gmail.com>
2024-08-01 20:49:26 +0200

    Import Angular Forms and Reactive Forms

--- yet-another-todo-app-frontend/src/app/table/table.module.ts
+++ yet-another-todo-app-frontend/src/app/table/table.module.ts
@@ -1,10 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule as AngularFormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TableModule as PrimeNgTableModule } from 'primeng/table';
 import { AppRoutingModule } from '../app-routing.module';
 import { HttpLoaderFactory } from '../app.module';
+import { FormsModule } from '../forms/forms.module';
 import { DialogService } from '../modals/services/dialog/dialog.service';
 import { MaterialModule } from '../shared/material.module';
 import { DateUtilsService } from '../shared/services/date-utils/date-utils.service';
@@ -30,6 +32,9 @@ import { TableComponent } from './components/table/table.component';
     BrowserModule,
     MaterialModule,
     PrimeNgTableModule,
+    AngularFormsModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [DateUtilsService, DialogService, TasksService],
   exports: [TableComponent, NewTableComponent],
