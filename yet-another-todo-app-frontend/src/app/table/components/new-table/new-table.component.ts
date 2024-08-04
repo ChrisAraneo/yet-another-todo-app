6427e5501fd3f5ccee9c5f589db4c4556143a97c
Krzysztof Pająk <chris.araneo@gmail.com>
2024-08-04 19:43:48 +0200

    Add basic table structure and logic

--- yet-another-todo-app-frontend/src/app/table/components/new-table/new-table.component.ts
+++ yet-another-todo-app-frontend/src/app/table/components/new-table/new-table.component.ts
@@ -1,8 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { DateUtilsService } from 'src/app/shared/services/date-utils/date-utils.service';
+import { TasksService } from 'src/app/shared/services/tasks/tasks.service';
+import { EndedTask, StartedTask } from '../../../../../../yet-another-todo-app-shared';
+import { TasksDataSource } from '../../table.types';
 
 @Component({
   selector: 'yata-new-table',
   templateUrl: './new-table.component.html',
   styleUrls: ['./new-table.component.scss'],
 })
-export class NewTableComponent {}
+export class NewTableComponent implements OnInit {
+  data!: Observable<TasksDataSource[]>;
+
+  constructor(
+    private readonly tasksService: TasksService,
+    private readonly dateUtilsService: DateUtilsService,
+  ) {}
+
+  ngOnInit(): void {
+    this.data = this.tasksService.getTasks().pipe(
+      map((tasks) =>
+        tasks.map((task) => ({
+          id: task.getId(),
+          shortId: task.getShortId(),
+          title: task.getTitle(),
+          description: task.getDescription(),
+          state: task.getState(),
+          creationDate: this.formatDate(task.getCreationDate()),
+          startDate: task instanceof StartedTask ? this.formatDate(task.getStartDate()) : '-',
+          endDate: task instanceof EndedTask ? this.formatDate(task.getEndDate()) : '-',
+        })),
+      ),
+    );
+  }
+
+  // TODO Pipe
+  private formatDate(date: Date): string {
+    return this.dateUtilsService.formatDate(date, 'yyyy-MM-dd HH:mm');
+  }
+}
