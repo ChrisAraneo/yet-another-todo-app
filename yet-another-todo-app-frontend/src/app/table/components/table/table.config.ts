0a7a70f9c58669f1642eb2013a30885b6e3ca52e
Krzysztof Pająk <chris.araneo@gmail.com>
2024-09-02 19:37:12 +0200

    Update table page size options const, add table max displayed paginator options

--- yet-another-todo-app-frontend/src/app/table/components/table/table.config.ts
+++ yet-another-todo-app-frontend/src/app/table/components/table/table.config.ts
@@ -1,4 +1,5 @@
-export const TABLE_PAGE_SIZE_OPTIONS: readonly number[] = [15, 30, 50] as const;
+export const TABLE_PAGE_SIZE_OPTIONS: readonly number[] = [15, 30, 75] as const;
+export const TABLE_MAX_DISPLAYED_PAGINATOR_OPTIONS = 9;
 export const TABLE_DISPLAYED_COLUMNS: readonly string[] = [
   'id',
   'title',
