7e34b4908d7ebde5cc8b549ea9c7315f7865963c
Krzysztof Pająk <chris.araneo@gmail.com>
2024-09-05 19:32:14 +0200

    feat(NewTable): change number of displayed paginator buttons on component resize

--- yet-another-todo-app-frontend/src/app/table/components/new-table/new-table.component.ts
+++ yet-another-todo-app-frontend/src/app/table/components/new-table/new-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { BehaviorSubject, combineLatest, map, Observable, Subject, tap } from 'rxjs';
 import { DateUtilsService } from 'src/app/shared/services/date-utils/date-utils.service';
 import { NavigationService } from 'src/app/shared/services/navigation/navigation.service';
@@ -6,7 +6,10 @@ import { TasksService } from 'src/app/shared/services/tasks/tasks.service';
 import { UNIT } from 'src/app/shared/styles/theme.__generated';
 import { EndedTask, StartedTask } from '../../../../../../yet-another-todo-app-shared';
 import { TasksDataSource } from '../../table.types';
-import { TABLE_PAGE_SIZE_OPTIONS } from '../table/table.config';
+import {
+  TABLE_MAX_DISPLAYED_PAGINATOR_OPTIONS,
+  TABLE_PAGE_SIZE_OPTIONS,
+} from '../table/table.config';
 
 @Component({
   selector: 'yata-new-table',
@@ -23,12 +26,24 @@ export class NewTableComponent implements OnInit {
   gridTemplateRows = {
     'grid-template-rows': `repeat(${this.pageSizeOptions[0]}, ${UNIT})`,
   };
+  maxDisplayedPageButtons: number = TABLE_MAX_DISPLAYED_PAGINATOR_OPTIONS;
 
   constructor(
     private readonly tasksService: TasksService,
     private readonly dateUtilsService: DateUtilsService,
     private readonly navigationService: NavigationService,
   ) {}
+
+  @HostListener('window:resize', ['$event.target.innerWidth'])
+  updateMaxDisplayedPageButtons(width: number): void {
+    if (width > 1450) {
+      this.maxDisplayedPageButtons = TABLE_MAX_DISPLAYED_PAGINATOR_OPTIONS;
+    } else if (width > 1200) {
+      this.maxDisplayedPageButtons = Math.ceil(TABLE_MAX_DISPLAYED_PAGINATOR_OPTIONS / 2);
+    } else {
+      this.maxDisplayedPageButtons = Math.ceil(TABLE_MAX_DISPLAYED_PAGINATOR_OPTIONS / 4);
+    }
+  }
 
   ngOnInit(): void {
     this.pageSize = new BehaviorSubject<number>(this.pageSizeOptions[0]); // TODO Store page sizes in param
@@ -57,6 +72,8 @@ export class NewTableComponent implements OnInit {
           .slice((currentPage - 1) * pageSize, currentPage * pageSize),
       ),
     );
+
+    this.updateMaxDisplayedPageButtons(window.innerWidth);
   }
 
   changePage(page: number): void {
