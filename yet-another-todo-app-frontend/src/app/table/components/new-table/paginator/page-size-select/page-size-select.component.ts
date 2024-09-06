bb9bbce56e17fbf9b1c1fabdc1891bbc0fc999fc
Krzysztof Pająk <chris.araneo@gmail.com>
2024-09-06 20:30:02 +0200

    feat(PageSizeSelect): create component

---
+++ yet-another-todo-app-frontend/src/app/table/components/new-table/paginator/page-size-select/page-size-select.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'yata-page-size-select',
+  templateUrl: './page-size-select.component.html',
+  styleUrls: ['./page-size-select.component.scss'],
+})
+export class PageSizeSelectComponent {}
