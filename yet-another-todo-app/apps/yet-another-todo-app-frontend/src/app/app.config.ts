d20c6605fa4efb4ee81db7e42a86046cf56dbba2
Krzysztof Pająk <chris.araneo@gmail.com>
2025-04-15 22:04:49 +0200

    feat: provide translate service

--- yet-another-todo-app/apps/yet-another-todo-app-frontend/src/app/app.config.ts
+++ yet-another-todo-app/apps/yet-another-todo-app-frontend/src/app/app.config.ts
@@ -2,10 +2,14 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideTranslateService } from '@ngx-translate/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideTranslateService({
+      defaultLanguage: 'en',
+    }),
   ],
 };
