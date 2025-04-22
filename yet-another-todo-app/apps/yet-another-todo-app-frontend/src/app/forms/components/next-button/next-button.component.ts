d3f6a0e6343a5dd4926919efc146cccec9558341
Krzysztof Pająk <chris.araneo@gmail.com>
2025-04-22 21:03:46 +0200

    feat(next-button): copy NextButton from previous Angular project

---
+++ yet-another-todo-app/apps/yet-another-todo-app-frontend/src/app/forms/components/next-button/next-button.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+import { NOOP } from '../../../shared/utils/noop.const';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+    selector: 'yata-next-button',
+    templateUrl: './next-button.component.html',
+    styleUrls: ['./next-button.component.scss'],
+    standalone: true,
+    imports: [MatIconModule]
+})
+export class NextButtonComponent {
+  @Input() next: (event?: any) => Promise<void> = NOOP;
+  @Input() icon?: string = 'navigate_next';
+
+  isLoading: boolean = false;
+
+  onButtonClick(event?: any): void {
+    this.isLoading = true;
+
+    this.next(event).then(() => (this.isLoading = false));
+  }
+}
