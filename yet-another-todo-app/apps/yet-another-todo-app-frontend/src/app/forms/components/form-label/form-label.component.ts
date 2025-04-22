7de496f6756910618e54cade5915636c6cee6f35
Krzysztof Pająk <chris.araneo@gmail.com>
2025-04-22 20:36:19 +0200

    feat(form-label): copy FormLabel from previous Angular project

---
+++ yet-another-todo-app/apps/yet-another-todo-app-frontend/src/app/forms/components/form-label/form-label.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'yata-form-label',
+    templateUrl: './form-label.component.html',
+    styleUrls: ['./form-label.component.scss'],
+    standalone: true
+})
+export class FormLabelComponent {
+  @Input() for: string = '';
+}
