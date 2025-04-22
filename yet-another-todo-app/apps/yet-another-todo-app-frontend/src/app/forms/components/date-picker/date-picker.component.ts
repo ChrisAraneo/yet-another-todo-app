31440a991b0f07ea9fae86a0bca96d14f657ddb5
Krzysztof Pająk <chris.araneo@gmail.com>
2025-04-22 20:28:41 +0200

    feat(date-picker): copy DatePicker from previous Angular project

---
+++ yet-another-todo-app/apps/yet-another-todo-app-frontend/src/app/forms/components/date-picker/date-picker.component.ts
@@ -0,0 +1,95 @@
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { FormLabelComponent } from '../form-label/form-label.component';
+import { DatePickerModule } from 'primeng/datepicker';
+
+@Component({
+  selector: 'yata-date-picker',
+  templateUrl: './date-picker.component.html',
+  styleUrls: ['./date-picker.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => DatePickerComponent),
+      multi: true,
+    },
+  ],
+  standalone: true,
+  imports: [FormLabelComponent, DatePickerModule, FormsModule],
+})
+export class DatePickerComponent implements ControlValueAccessor {
+  @Input() label: string = '';
+  @Input() range: boolean = false;
+
+  model?: Date | (Date | null)[];
+
+  value: string | string[] | null;
+  isDisabled: boolean;
+
+  changed?: (value: string | string[] | null) => void;
+  touched?: () => void;
+
+  constructor() {
+    this.value = null;
+    this.isDisabled = false;
+  }
+
+  onChangeDate(event: Date | (Date | null)[] | unknown): void {
+    if (this.range && this.isDateOrNullArray(event)) {
+      const dates = event.filter((item: Date | null) => item !== null);
+
+      this.value = dates
+        .map((item) => item?.toISOString())
+        .filter((item) => !!item) as string[];
+    } else {
+      this.value = event instanceof Date ? event.toISOString() : null;
+    }
+
+    this.changed && this.changed(this.value);
+  }
+
+  onBlur(): void {
+    this.touched && this.touched();
+  }
+
+  writeValue(value: string | string[] | null): void {
+    this.value = value;
+
+    this.updateModel(value);
+  }
+
+  registerOnChange(fn: any): void {
+    this.changed = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.touched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+  private updateModel(value: string | string[] | null): void {
+    if (value === null) {
+      this.model = undefined;
+    } else if (typeof value === 'string') {
+      this.model = new Date(value);
+    } else if (this.isStringArray(value)) {
+      this.model = value.map((item) => new Date(item));
+    }
+  }
+
+  private isStringArray(value: unknown): value is string[] {
+    return (
+      Array.isArray(value) && !value.find((item) => !(typeof item === 'string'))
+    );
+  }
+
+  private isDateOrNullArray(value: unknown): value is (Date | null)[] {
+    return (
+      Array.isArray(value) &&
+      !value.find((item) => !(item instanceof Date || item === null))
+    );
+  }
+}
