1c36f24c2c754840b63322c958d3377fecba778d
ChrisAraneo <chris.araneo@gmail.com>
2025-04-29 15:38:34 +0200

    feat(eslint): update config

--- yet-another-todo-app/eslint.config.mjs
+++ yet-another-todo-app/eslint.config.mjs
@@ -2,27 +2,69 @@ import eslint from '@eslint/js';
 import eslintPluginJsonc from 'eslint-plugin-jsonc';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import tseslint from 'typescript-eslint';
+import angular from 'angular-eslint';
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
-  ...eslintPluginJsonc.configs['flat/recommended-with-jsonc'],
+  // TODO Regular .ts files
   {
+    files: [
+      '**/*.component.ts',
+      '**/*.component.spec.ts',
+      '**/*.directive.ts',
+      '**/*.directive.spec.ts',
+    ],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.strict,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
     plugins: {
       'simple-import-sort': simpleImportSort,
     },
+    processor: angular.processInlineTemplates,
     rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'yata',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'yata',
+          style: 'kebab-case',
+        },
+      ],
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
-      'jsonc/no-comments': 'error',
-      'jsonc/sort-keys': 'error',
       '@typescript-eslint/no-extraneous-class': 'off',
     },
   },
   {
+    files: ['**/*.component.html'],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  },
+  {
+    files: ['**/*.json'],
+    extends: [...eslintPluginJsonc.configs['flat/recommended-with-jsonc']],
+    rules: {
+      'jsonc/no-comments': 'error',
+      'jsonc/sort-keys': 'error',
+    },
+  },
+  {
     ignores: [
-      'node_modules/',
+      '.nx/workspace-data',
+      'node_modules',
       '.git/',
       'reports/',
       '.stryker-tmp/',
@@ -30,6 +72,8 @@ export default tseslint.config(
       'package-lock.json',
       'apps/**/package.json',
       'apps/**/package-lock.json',
+      'apps/**/node_modules/',
+      'apps/**/.angular/',
     ],
   },
 );
