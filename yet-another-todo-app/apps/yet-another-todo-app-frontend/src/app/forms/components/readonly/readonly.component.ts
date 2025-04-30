95f67767a2e15cc891889879f0d3728154023c49
ChrisAraneo <chris.araneo@gmail.com>
2025-04-30 17:16:39 +0200

    feat(readonly): copy component from previous Angular project

---
+++ yet-another-todo-app/apps/yet-another-todo-app-frontend/src/app/forms/components/readonly/readonly.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { FormLabelComponent } from '../form-label/form-label.component';
+
+@Component({
+    selector: 'yata-readonly',
+    templateUrl: './readonly.component.html',
+    styleUrls: ['./readonly.component.scss'],
+    standalone: true,
+    imports: [FormLabelComponent]
+})
+export class ReadonlyComponent {
+  @Input() label: string = '';
+  @Input() value?: string | null = '';
+}
