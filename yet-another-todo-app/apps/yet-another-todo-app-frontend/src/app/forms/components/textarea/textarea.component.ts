63cf4884eff0fdc592de8ebe60830cb39c1082ca
ChrisAraneo <chris.araneo@gmail.com>
2025-05-04 11:47:23 +0200

    feat(textarea): copy component from previous Angular project

---
+++ yet-another-todo-app/apps/yet-another-todo-app-frontend/src/app/forms/components/textarea/textarea.component.ts
@@ -0,0 +1,63 @@
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { FormLabelComponent } from '../form-label/form-label.component';
+import { TextareaModule } from 'primeng/textarea';
+import { ErrorTooltipDirective } from '../../directives/error-tooltip/error-tooltip.directive';
+
+@Component({
+  selector: 'yata-textarea',
+  templateUrl: './textarea.component.html',
+  styleUrls: ['./textarea.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TextareaComponent),
+      multi: true,
+    },
+  ],
+  standalone: true,
+  imports: [FormLabelComponent, TextareaModule, ErrorTooltipDirective],
+})
+export class TextareaComponent implements ControlValueAccessor {
+  @Input() label: string = '';
+  @Input() rows: number = 3;
+  @Input() cols: number = 30;
+  @Input() maxLength: number = 9999;
+
+  value: string;
+  isDisabled: boolean;
+
+  changed?: (value: string) => void;
+  touched?: () => void;
+
+  constructor() {
+    this.value = '';
+    this.isDisabled = false;
+  }
+
+  onChange(event: Event): void {
+    const value: string = (<HTMLInputElement>event.target).value;
+
+    this.changed && this.changed(value);
+  }
+
+  onBlur(): void {
+    this.touched && this.touched();
+  }
+
+  writeValue(value: string): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.changed = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.touched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+}
