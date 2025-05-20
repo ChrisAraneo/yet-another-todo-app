8a8e5b8f552d1d5a9777d4217a985c0155306d02
Krzysztof Pająk <chris.araneo@gmail.com>
2025-05-20 20:39:55 +0200

    chore: update ignores

--- yet-another-todo-app/eslint.config.mjs
+++ yet-another-todo-app/eslint.config.mjs
@@ -3,4 +3,8 @@ import config from '@chris.araneo/eslint-config';
 
 export default tslint.config({
   extends: [config],
+  ignores: [
+    'apps/**/package.json',
+    'apps/yet-another-todo-app-mock/assets/store.json',
+  ],
 });
