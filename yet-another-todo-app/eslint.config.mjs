ded9edbb235f5a6a9daf73a5c067bc5f6c773663
Krzysztof Pająk <chris.araneo@gmail.com>
2025-05-31 17:51:00 +0200

    chore(eslint): update eslint config

--- yet-another-todo-app/eslint.config.mjs
+++ yet-another-todo-app/eslint.config.mjs
@@ -1,10 +1,36 @@
-import tslint from 'typescript-eslint';
-import config from '@chris.araneo/eslint-config';
+import createConfig from '@chris.araneo/eslint-config';
 
-export default tslint.config({
-  extends: [config],
-  ignores: [
-    'apps/**/package.json',
-    'apps/yet-another-todo-app-mock/assets/store.json',
-  ],
-});
+const jsons = [
+  '.vscode/*.json',
+  'apps/**/public/**/*.json',
+  'apps/**/assets/**/*.json',
+  'apps/**/src/**/*.json',
+  'apps/**/tsconfig.json',
+  'apps/**/tsconfig.lib.json',
+  '.prettierrc.json',
+  'angular.json',
+  'tsconfig.app.json',
+  'tsconfig.json',
+  'tsconfig.spec.json',
+  'stryker.config.json',
+  'nx.json',
+];
+
+const sources = ['apps/**/*.ts'];
+
+const htmls = ['apps/**/*.html'];
+
+const ignored = [
+  'node_modules/',
+  'apps/**/.angular/',
+  'apps/**/node_modules/',
+  'apps/**/package.json',
+  'apps/**/package-lock.json',
+  '.stryker-tmp/',
+  'dist/',
+  'reports/',
+  'package.json',
+  'package-lock.json',
+];
+
+export default createConfig(jsons, sources, htmls, ignored);
