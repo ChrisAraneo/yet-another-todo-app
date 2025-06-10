5fda226dc19a60c03f8e8d04a705f4b2d6d0766d
Krzysztof Pająk <chris.araneo@gmail.com>
2025-06-10 21:50:40 +0200

    feat(timeline-header): copy component from previous project dir, make standalone

---
+++ yet-another-todo-app/apps/yet-another-todo-app-frontend/src/app/timeline/components/timeline/timeline-header/timeline-header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'yata-timeline-header',
+  templateUrl: './timeline-header.component.html',
+  styleUrl: './timeline-header.component.scss',
+  standalone: true,
+})
+export class TimelineHeaderComponent {}
