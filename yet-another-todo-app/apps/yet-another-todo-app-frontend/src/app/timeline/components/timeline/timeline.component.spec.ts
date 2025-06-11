f1b683d5c55b7a33a844f919eb8ab16555d1b36d
Krzysztof Pająk <chris.araneo@gmail.com>
2025-06-11 19:09:41 +0200

    feat: copy component, service and interfaces from previous project dir, make standalone

---
+++ yet-another-todo-app/apps/yet-another-todo-app-frontend/src/app/timeline/components/timeline/timeline.component.spec.ts
@@ -0,0 +1,51 @@
+/* eslint-disable simple-import-sort/imports */
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { TranslatePipe } from '@ngx-translate/core';
+import { MockPipe, MockProvider } from 'ng-mocks';
+import { of } from 'rxjs';
+
+import { TimelineComponent } from './timeline.component';
+import { TasksService } from '../../../shared/services/tasks/tasks.service';
+
+describe('TimelineComponent', () => {
+  let component: TimelineComponent;
+  let fixture: ComponentFixture<TimelineComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TimelineComponent, MockPipe(TranslatePipe)],
+      imports: [MatDialogModule, NoopAnimationsModule, StoreModule.forRoot({})],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: [] },
+        MockProvider(TasksService, {
+          getTasks: () => of([]),
+        }),
+        provideHttpClient(withInterceptorsFromDi()),
+        provideHttpClientTesting(),
+      ],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TimelineComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ yet-another-todo-app/apps/yet-another-todo-app-frontend/src/app/timeline/services/timeline-task-manager.service.ts
@@ -0,0 +1,216 @@
+import { Injectable } from '@angular/core';
+import { DateUtilsService } from 'src/app/shared/services/date-utils/date-utils.service';
+
+import {
+  EndedTask,
+  PendingTask,
+  StartedTask,
+  Task,
+  TaskState,
+} from '../../../../../yet-another-todo-app-shared';
+import { TimelineColumn } from '../components/timeline/timeline-content/timeline-content.types';
+
+interface Column {
+  tasks: Task[];
+  position: number;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TimelineTaskManagerService {
+  constructor(private readonly dateUtilsService: DateUtilsService) {}
+
+  mapTasksToTimelineColumns(
+    tasks: Task[],
+    today: Date,
+    timelineStartDate: Date,
+    timelineEndDate: Date,
+    statesFilter: TaskState[],
+    statesOrder: TaskState[],
+  ): TimelineColumn[] {
+    this.throwErrorWhenStartDateIsAfterEndDate(
+      timelineStartDate,
+      timelineEndDate,
+    );
+
+    const tasksInSelectedPeriod = tasks.filter((task: Task) =>
+      this.isTaskInSelectedPeriod(
+        task,
+        timelineStartDate,
+        timelineEndDate,
+        today,
+      ),
+    );
+
+    const filteredTasks: Task[] = tasksInSelectedPeriod.filter((task: Task) =>
+      this.includesState(statesFilter, task.getState()),
+    );
+
+    const columns = this.organizeTasksIntoColumns(
+      filteredTasks,
+      timelineStartDate,
+      timelineEndDate,
+      today,
+    );
+
+    this.sortTasksInColumns(columns, statesOrder);
+
+    return this.mapColumnsToTimelineColumns(columns);
+  }
+
+  private isTaskInSelectedPeriod(
+    task: Task,
+    timelineStartDate: Date,
+    timelineEndDate: Date,
+    today: Date,
+  ): boolean {
+    const isTodayInDateRange =
+      today.valueOf() >= timelineStartDate.valueOf() &&
+      today.valueOf() <= timelineEndDate.valueOf();
+    const isPendingTask = task instanceof PendingTask;
+
+    if (isTodayInDateRange && isPendingTask) {
+      return true;
+    }
+
+    const startDate = task instanceof StartedTask ? task.getStartDate() : today;
+    const endDate = task instanceof EndedTask ? task.getEndDate() : today;
+
+    return (
+      startDate.valueOf() >= timelineStartDate.valueOf() &&
+      startDate.valueOf() <= timelineEndDate.valueOf() &&
+      endDate.valueOf() >= timelineStartDate.valueOf() &&
+      endDate.valueOf() <= timelineEndDate.valueOf()
+    );
+  }
+
+  private includesState(array: TaskState[], state: TaskState): boolean {
+    return array.some((item) => item.toString() === state.toString());
+  }
+
+  private organizeTasksIntoColumns(
+    tasks: Task[],
+    timelineStartDate: Date,
+    timelineEndDate: Date,
+    today: Date,
+  ): Column[] {
+    const columnTasksMap = new Map<
+      number,
+      { tasks: Task[]; position: number }
+    >();
+
+    for (const task of tasks) {
+      let placementDate: Date | undefined;
+
+      if (task instanceof EndedTask) {
+        placementDate = task.getEndDate();
+      } else if (
+        task instanceof StartedTask &&
+        +today >= +task.getStartDate()
+      ) {
+        placementDate = today;
+      } else if (task instanceof StartedTask && +today < +task.getStartDate()) {
+        placementDate = task.getStartDate();
+      } else {
+        placementDate = today;
+      }
+
+      if (
+        (placementDate instanceof Date &&
+          +placementDate >=
+            +this.dateUtilsService.getNextDay(timelineEndDate)) ||
+        placementDate === undefined
+      ) {
+        continue;
+      }
+
+      const key =
+        this.dateUtilsService.getNumberOfDaysBetweenDates(
+          timelineStartDate,
+          placementDate,
+        ) - 1;
+
+      if (columnTasksMap.has(key)) {
+        columnTasksMap.set(key, {
+          tasks: [...(columnTasksMap.get(key)?.tasks || []), task],
+          position: key,
+        });
+      } else {
+        columnTasksMap.set(key, {
+          tasks: [task],
+          position: key,
+        });
+      }
+    }
+
+    return [...columnTasksMap.values()].sort((a, b) => a.position - b.position);
+  }
+
+  private sortTasksInColumns(
+    columns: Column[],
+    statesOrder: TaskState[],
+  ): void {
+    const statesPriorities = new Map<string, number>();
+
+    statesOrder.forEach((value: TaskState, index: number) => {
+      statesPriorities.set(value.toString(), index);
+    });
+
+    columns.forEach((column: Column) => {
+      column.tasks.sort((a: Task, b: Task) => {
+        const stateA = a.getState().toString();
+        const stateB = b.getState().toString();
+        const priorityA = statesPriorities.get(stateA);
+        const priorityB = statesPriorities.get(stateB);
+
+        if (this.isUndefined(priorityA)) {
+          return 1;
+        }
+
+        if (this.isUndefined(priorityB)) {
+          return -1;
+        }
+
+        const creationDateDiff =
+          b.getCreationDate().valueOf() - a.getCreationDate().valueOf();
+
+        if (priorityA === priorityB && creationDateDiff !== 0) {
+          return creationDateDiff;
+        } else if (priorityA === priorityB && creationDateDiff === 0) {
+          return a.getId().localeCompare(b.getId());
+        }
+
+        return priorityA! - priorityB!;
+      });
+    });
+  }
+
+  private isUndefined(x: unknown): boolean {
+    return x === undefined;
+  }
+
+  private mapColumnsToTimelineColumns(columns: Column[]): TimelineColumn[] {
+    return columns.map((column: Column, index: number, array: Column[]) => {
+      if (index === 0) {
+        return {
+          tasks: column.tasks,
+          leftMargin: column.position,
+        };
+      }
+      return {
+        tasks: column.tasks,
+        leftMargin: column.position - array[index - 1].position - 1,
+      };
+    });
+  }
+
+  private throwErrorWhenStartDateIsAfterEndDate(
+    startDate: Date,
+    endDate: Date,
+  ): void {
+    if (+startDate > +endDate) {
+      throw new Error('The start date is after the end date');
+    }
+  }
+}
