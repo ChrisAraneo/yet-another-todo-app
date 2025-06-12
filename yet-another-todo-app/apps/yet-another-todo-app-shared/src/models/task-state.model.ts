30706d7fd94d505e2bb4cd16f791946b1dd95033
Krzysztof Pająk <chris.araneo@gmail.com>
2025-06-12 18:35:32 +0200

    fix: import from index

--- yet-another-todo-app/apps/yet-another-todo-app-shared/src/models/task-state.model.ts
+++ yet-another-todo-app/apps/yet-another-todo-app-shared/src/models/task-state.model.ts
@@ -1,4 +1,10 @@
-import { DANGER_COLOR, DISABLED_COLOR, STANDARD_TEXT_COLOR, SUCCESS_COLOR, WARNING_COLOR } from 'src/themes/theme.__generated';
+import {
+  DANGER_COLOR,
+  DISABLED_COLOR,
+  STANDARD_TEXT_COLOR,
+  SUCCESS_COLOR,
+  WARNING_COLOR,
+} from '../..';
 import { v4 as uuidv4 } from 'uuid';
 
 export abstract class TaskState {
