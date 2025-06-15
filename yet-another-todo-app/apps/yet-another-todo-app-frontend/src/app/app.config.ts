599036228df27a9530b6b8733a5e3148d4f6b1b9
Krzysztof Pająk <chris.araneo@gmail.com>
2025-06-15 17:46:58 +0200

    fix: provide i18n using translate http loader

--- yet-another-todo-app/apps/yet-another-todo-app-frontend/src/app/app.config.ts
+++ yet-another-todo-app/apps/yet-another-todo-app-frontend/src/app/app.config.ts
@@ -1,14 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
-import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
 import Aura from '@primeng/themes/aura';
 import { providePrimeNG } from 'primeng/config';
 import { provideStore } from '@ngrx/store';
 import { environment } from '../environments/environment';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
+  new TranslateHttpLoader(http, '/public/i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,6 +23,11 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideTranslateService({
       defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient],
+      },
     }),
     provideAnimationsAsync(),
     providePrimeNG({
