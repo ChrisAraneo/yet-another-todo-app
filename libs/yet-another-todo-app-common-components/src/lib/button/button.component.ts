5523788e0c6ceb5658d89b834368a59444c1b5ae
Krzysztof Pająk <chris.araneo@gmail.com>
2025-12-07 14:02:06 +0100

    feat(button): implement new ButtonComponent

--- libs/yet-another-todo-app-common-components/src/lib/button/button.component.ts
+++ libs/yet-another-todo-app-common-components/src/lib/button/button.component.ts
@@ -1,39 +1,68 @@
-import { Component, Input, OnDestroy } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { trigger, style, transition, animate } from '@angular/animations';
+import {
+  trigger,
+  style,
+  transition,
+  animate,
+  keyframes,
+} from '@angular/animations';
 import { Ripple } from './button.interfaces';
-import { RIPPLE_ANIMATION_DURATION_MS } from './button.consts';
+import {
+  RIPPLE_ANIMATION_DURATION_MS,
+  RIPPLE_ANIMATION_OFFSET_DURATION_MS,
+} from './button.consts';
+import { noop } from 'lodash';
 
 @Component({
   selector: 'yata-button',
   standalone: true,
-  imports: [CommonModule],``
+  imports: [CommonModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss',
   animations: [
     trigger('rippleAnimation', [
       transition(':enter', [
-        style({ transform: 'scale(0)', opacity: 1 }),
-        animate(`${RIPPLE_ANIMATION_DURATION_MS}ms ease-out`, style({ transform: 'scale(4)', opacity: 0 }))
-      ])
-    ])
-  ]
+        animate(
+          `${RIPPLE_ANIMATION_DURATION_MS}ms ease-out`,
+          keyframes([
+            style({ transform: 'scale(0)', opacity: 0.2, offset: 0 }),
+            style({
+              transform: 'scale(4)',
+              opacity: 0,
+              offset:
+                (RIPPLE_ANIMATION_DURATION_MS -
+                  RIPPLE_ANIMATION_OFFSET_DURATION_MS) /
+                RIPPLE_ANIMATION_DURATION_MS,
+            }),
+            style({ transform: 'scale(4)', opacity: 0, offset: 1 }),
+          ]),
+        ),
+      ]),
+    ]),
+  ],
 })
-export class ButtonComponent implements OnDestroy {
-  ngOnDestroy(): void {
-      throw new Error('Method not implemented.');
-  }
+export class ButtonComponent {
   @Input() variant: 'primary' | 'danger' | 'ghost' = 'primary';
   @Input() disabled = false;
   @Input() type: 'button' | 'submit' | 'reset' = 'button';
+  @Input() click: () => void = noop;
 
   ripples: Ripple[] = [];
 
-  createRipple(event: MouseEvent): void {
-    if (this.disabled) {
-        return;
-    };
+  onClick(event: MouseEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
 
+    if (this.disabled) {
+      return;
+    }
+
+    this.createRipple(event);
+    this.click();
+  }
+
+  private createRipple(event: MouseEvent): void {
     const button = event.currentTarget as HTMLElement;
     const rect = button.getBoundingClientRect();
     const size = Math.max(rect.width, rect.height) * 2;
@@ -45,7 +74,7 @@ export class ButtonComponent implements OnDestroy {
     this.ripples.push(ripple);
 
     setTimeout(() => {
-      this.ripples = this.ripples.filter(r => r !== ripple);
-    }, RIPPLE_ANIMATION_DURATION_MS);
+      this.ripples = this.ripples.filter((r) => r !== ripple);
+    }, RIPPLE_ANIMATION_DURATION_MS - RIPPLE_ANIMATION_OFFSET_DURATION_MS);
   }
 }
