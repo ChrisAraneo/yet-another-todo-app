ed8e280f6ad210afce4141cbcac87015890b4f83
Krzysztof Pająk <chris.araneo@gmail.com>
2025-12-11 21:57:42 +0100

    test(spinner): update tests and add provideNoopAnimations provider

--- libs/yet-another-todo-app-common-components/src/lib/components/spinner/spinner.component.spec.ts
+++ libs/yet-another-todo-app-common-components/src/lib/components/spinner/spinner.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { SpinnerComponent } from './spinner.component';
 
 describe('SpinnerComponent', () => {
@@ -8,6 +9,7 @@ describe('SpinnerComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SpinnerComponent],
+      providers: [provideNoopAnimations()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SpinnerComponent);
