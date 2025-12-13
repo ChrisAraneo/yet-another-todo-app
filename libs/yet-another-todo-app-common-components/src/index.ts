f470984521313ec519c6d7d263e312f0fd24d99a
Krzysztof Pająk <chris.araneo@gmail.com>
2025-12-14 00:12:36 +0100

    chore: export Variant type

--- libs/yet-another-todo-app-common-components/src/index.ts
+++ libs/yet-another-todo-app-common-components/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/components/button/button.component';
 export * from './lib/components/spinner/spinner.component';
 export * from './lib/directives/variant/variant.directive';
+export * from './lib/types/variant.type';
